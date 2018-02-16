a5489d95d321356f804efcbbf56f43a11e617958
JP Smith <jp@writes.co.de>
2018-02-16 14:24:45 -0500

    switch echidna-test to use hedgehog

--- src/Main.hs
+++ src/Main.hs
@@ -2,13 +2,9 @@
 
 module Main where
 
-import Data.List (intercalate)
-import Data.Monoid ((<>))
-import Data.Text (Text, unpack)
+import Control.Monad (forM_)
+import Hedgehog (check)
 import System.Environment (getArgs)
-
-import EVM.ABI
-import EVM
 
 import Echidna.Exec
 import Echidna.Solidity
@@ -16,11 +12,6 @@ import Echidna.Solidity
 main :: IO ()
 main = getArgs >>= \case
   []  -> putStrLn "Please provide a solidity file to analyze"
-  f:_ -> do (v,a,t) <- loadSolidity f
-            mapM_ (test v a) t
-
-test :: VM -> [(Text, [AbiType])] -> Text -> IO ()
-test v a t = do putStrLn $ unpack ("[*] Checking test " <> t <> "...")
-                res <- fuzz 10 10000 a v (\x -> return $ checkETest x t) 
-                putStrLn $ maybe "[+] Test passed!"
-                  (("[!] Test failed! Calls used: " ++) . intercalate "; ") res
+  f:_ -> loadSolidity f >>= \(v,a,ts) -> forM_ ts $ \t -> do
+    putStrLn ("[*] Checking test " ++ show t)
+    check $ ePropertySeq v a (`checkETest` t) 10
