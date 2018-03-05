00ad5c8f3a6154cad3835b0dc2d5cf187e4258d5
ggrieco-tob <gustavo.grieco@trailofbits.com>
2018-03-05 16:46:06 -0500

    modified the revert example to use ePropertySeq

--- examples/revert/Revert.hs
+++ examples/revert/Revert.hs
@@ -1,21 +1,16 @@
 module Main where
 
-import Control.Lens
-import Control.Monad
+import Hedgehog hiding (checkParallel)
+import Hedgehog.Internal.Property (GroupName(..), PropertyName(..))
+import System.Environment         (getArgs)
 
-import EVM
 import Echidna.Exec
+import Echidna.Internal.Runner (checkParallel)
 import Echidna.Solidity
+import Control.Monad (forever)
 
 main :: IO ()
-main = do (v,a,_) <- loadSolidity "solidity/revert.sol"
-          forever $ fcontract v a
-
---fcontract :: VM -> [SolSignature] -> IO ()
-fcontract v a = do
-          res     <- fuzz 2 100 a v worked
-          putStr $ maybe "."
-                           (("\nTests failed! Counterexample: " ++) . show) res
-  where worked c = case c ^. result of (Just (VMSuccess _)) -> return True
-                                       (Just (VMFailure Revert)) -> return False
-                                       _                    -> return True
+main = do (v,a,ts) <- loadSolidity "solidity/revert.sol"
+          let prop t = (PropertyName $ show t, ePropertySeq v a (`checkRTest` t) 10)
+          _ <- checkParallel . Group (GroupName "revert.sol") $ map prop ts
+          return ()
