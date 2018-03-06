4d5888b80bb4a906a533f99e71d0fe7c7f15e5fb
ggrieco-tob <gustavo.grieco@trailofbits.com>
2018-03-06 11:57:12 -0500

    fixed unused imports

--- examples/revert/Revert.hs
+++ examples/revert/Revert.hs
@@ -2,12 +2,10 @@ module Main where
 
 import Hedgehog hiding (checkParallel)
 import Hedgehog.Internal.Property (GroupName(..), PropertyName(..))
-import System.Environment         (getArgs)
 
 import Echidna.Exec
 import Echidna.Internal.Runner (checkParallel)
 import Echidna.Solidity
-import Control.Monad (forever)
 
 main :: IO ()
 main = do (v,a,ts) <- loadSolidity "solidity/revert.sol"
