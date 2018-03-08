7829729eb791caf5946b411372cb4357e56be1fe
JP Smith <jp@writes.co.de>
2018-03-08 11:30:25 +0100

    add exercise

---
+++ exercises/testme.sol
@@ -0,0 +1,24 @@
+pragma solidity ^0.4.16;
+
+contract Canal {
+  bool private first_gate_up=false;
+  bool private second_gate_up=true;
+
+  function lower(bool first_gate) returns (bool){
+    if(first_gate) {
+      if(second_gate_up) {
+        first_gate_up = false;
+        return(true);
+      } else {
+        return(false);
+      }
+    } else {
+      if(first_gate_up) {
+        second_gate_up = false;
+        return true;
+      } else {
+        return(false);
+      }
+    }
+  }
+}
