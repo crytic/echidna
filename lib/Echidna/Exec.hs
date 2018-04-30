4180200a50c9070112fc5a0f8d70a71cc30384f1
Ben Perez <benperez1227@gmail.com>
2018-04-30 15:23:52 -0400

    Added functionality for taking the top performing tests from each round and feeding them into the next. Command line args now include number of epochs, will fix to be optional

--- lib/Echidna/Exec.hs
+++ lib/Echidna/Exec.hs
@@ -1,8 +1,7 @@
 {-# LANGUAGE FlexibleContexts, KindSignatures, LambdaCase, StrictData #-}
 
 module Echidna.Exec (
-    ExecEnv(..)
-  , checkETest
+    checkETest
   , eCommand
   , eCommandCoverage
   , ePropertySeq
@@ -10,22 +9,21 @@ module Echidna.Exec (
   , execCall
   , execCallCoverage
   , fuzz
-  , mutateGoodInputs
+  , setCoverage
   , module Echidna.Internal.Runner
   ) where
 
-import Control.Concurrent.MVar    (MVar, takeMVar, putMVar)
-import Control.Concurrent.Chan    (Chan, writeChan, readChan)
+import Control.Concurrent.MVar    (MVar, modifyMVar_)
 import Control.Lens               ((^.), (.=), use)
-import Control.Monad              (forM_, replicateM, forever)
+import Control.Monad              (forM_, replicateM)
 import Control.Monad.Catch        (MonadCatch)
 import Control.Monad.IO.Class     (MonadIO, liftIO)
 import Control.Monad.State.Strict (MonadState, StateT, evalState, evalStateT, execState, runState)
 import Control.Monad.Reader       (MonadReader, ReaderT, runReaderT, ask)
 import Data.IORef                 (IORef, modifyIORef', newIORef, readIORef)
-import Data.List                  (intercalate)
+import Data.List                  (intercalate, foldl', delete, sortOn)
 import Data.Maybe                 (listToMaybe)
-import Data.Set                   (Set, insert, union, size, difference)
+import Data.Set                   (Set, insert, union, size, intersection)
 import Data.Text                  (Text)
 import Data.Typeable              (Typeable)
 import Data.Vector                (fromList)
@@ -43,37 +41,23 @@ import EVM.ABI      (AbiValue(..), abiCalldata, abiValueType, encodeAbiValue)
 import EVM.Concrete (Blob(..))
 import EVM.Exec     (exec)
 
-import Echidna.ABI (SolCall, SolSignature, displayAbiCall, encodeSig, genInteractions)
-import Echidna.Internal.Runner
+import Echidna.ABI (SolCall, SolSignature, displayAbiCall, encodeSig, genInteractions, mutateCall)
+import Echidna.Internal.Runner 
 
-  
-data ExecEnv = ExecEnv
-  { coverage :: Coverage --Cumulative coverage accross all tests
-  , recent   :: [Int] -- Coverage for previous 10 tests
-  , avg      :: Float -- Moving average of coverage for prior tests
-  }  
 
-type Coverage    = Set Int      
-type CoverageRef = IORef Coverage
+--------------------------------------------------------------------
+-- COVERAGE HANDLING
 
---This doesn't do anything right now... but it will be a thread that processes inputs
---deemed interesting from a channel
-mutateGoodInputs :: Chan [SolSignature] -> IO ()
-mutateGoodInputs c = forever $ do
-  input <- readChan c
-  return ()
+type CoverageInfo = (SolCall, Set Int)
+type CoverageRef  = IORef CoverageInfo
 
-updateExecEnv :: ExecEnv -> Coverage -> ExecEnv
-updateExecEnv ExecEnv{coverage = c, recent = r, avg = a} cov = ExecEnv { coverage = c', recent = r', avg = a'}
-  where c' = union c cov
-        r' = (size cov) : (take 5 r)
-        a' = fromIntegral $ (sum r) `div` (length r)
 
--- Use to determine if input is worth handing off to the "good input" pool for mutation
--- Currently just sees if current input has higher than avg new pc's
-isInteresting :: ExecEnv -> Coverage -> Bool
-isInteresting e c = if fromIntegral (size new) > avg e then True else False
-  where new = coverage e `difference` c
+-- At the beginning of each epoch, we take the
+-- top 100 calls from the last w/r/t coverage
+setCoverage :: [CoverageInfo] -> [SolCall]
+setCoverage [] = []
+setCoverage xs = take 100 $ map fst (sortOn (size . snd) xs)
+
 
 execCallUsing :: MonadState VM m => m VMResult -> SolCall -> m VMResult
 execCallUsing m (t,vs) = cleanUp >> (state . calldata .= cd >> m) where
@@ -87,15 +71,17 @@ execCall = execCallUsing exec
 
 
 execCallCoverage :: (MonadState VM m, MonadReader CoverageRef m, MonadIO m) => SolCall -> m VMResult
-execCallCoverage = execCallUsing (go mempty) where
+execCallCoverage sol = execCallUsing (go mempty) sol where
   go c = use result >>= \case
     Just x -> do ref <- ask
-                 liftIO $ modifyIORef' ref (union c)
+                 liftIO $ modifyIORef' ref (\_ -> (sol, c))
                  return x
     _      -> do current <- use $ state . pc
                  S.state (runState exec1)
                  go $ insert current c
 
+-------------------------------------------------------------------
+-- Fuzzing and Hedgehog Init
 
 fuzz :: MonadIO m
      => Int                 -- Call sequence length
@@ -134,36 +120,38 @@ instance HTraversable VMAction where
 
 
 eCommandUsing :: (MonadGen n, MonadTest m, Typeable a)
-              => (VMAction Concrete -> m a)
+              => n SolCall
+              -> (VMAction Concrete -> m a)
               -> (VM -> Bool)
-              -> [SolSignature]
               -> Command n m VMState
-eCommandUsing o p ts = Command (\_ -> pure $ Call <$> genInteractions ts) o
+eCommandUsing gen ex p = Command (\_ -> pure $ Call <$> gen) ex
   [ Ensure $ \_ (VMState v) _ _ -> assert $ p v
   , Update $ \(VMState v) (Call c) _ -> VMState $ execState (execCall c) v
   ]
   
 
-eCommand :: (MonadGen n, MonadTest m) => (VM -> Bool) -> [SolSignature] -> Command n m VMState
-eCommand = eCommandUsing (\_ -> pure ())
+eCommand :: (MonadGen n, MonadTest m) => n SolCall -> (VM -> Bool) -> Command n m VMState
+eCommand x y = eCommandUsing x (\_ -> pure ()) y
 
 
 eCommandCoverage :: (MonadGen n, MonadTest m, MonadState VM m, MonadReader CoverageRef m, MonadIO m)
-                 => (VM -> Bool) -> [SolSignature] -> Command n m VMState
-eCommandCoverage = eCommandUsing $ \(Call c) -> execCallCoverage c
+                 => [SolCall] -> (VM -> Bool) -> [SolSignature] -> [Command n m VMState]
+eCommandCoverage cov p ts = case cov of
+  [] -> [eCommandUsing (genInteractions ts) (\(Call c) -> execCallCoverage c) p]
+  xs -> map (\x -> eCommandUsing (mutateCall x) (\(Call c) -> execCallCoverage c) p) xs
 
 ePropertyUsing :: (MonadCatch m, MonadTest m)
-             => Command Gen m VMState
+             => [Command Gen m VMState]
              -> (m () -> PropertyT IO ())
              -> VM          
              -> Int        
              -> Property
-ePropertyUsing c f v n = mapConfig (\x -> x {propertyTestLimit = 10000}) . property $
+ePropertyUsing cs f v n = mapConfig (\x -> x {propertyTestLimit = 10000}) . property $
   f . executeSequential (VMState v) =<< forAllWith printCallSeq
-  (sequential (linear 1 n) (VMState v) [c]) where
-    printCallSeq = ("Call sequence: " ++) . intercalate "\n               " .
-      map showCall . sequentialActions
-    showCall (Action i _ _ _ _ _) = show i ++ ";"
+  (sequential (linear 1 n) (VMState v) cs)
+  where printCallSeq = ("Call sequence: " ++) . intercalate "\n               " .
+          map showCall . sequentialActions
+        showCall (Action i _ _ _ _ _) = show i ++ ";"
 
 
 ePropertySeq :: (VM -> Bool)   -- Predicate to fuzz for violations of
@@ -171,27 +159,26 @@ ePropertySeq :: (VM -> Bool)   -- Predicate to fuzz for violations of
              -> VM             -- Initial state
              -> Int            -- Max actions to execute
              -> Property
-ePropertySeq p ts = ePropertyUsing (eCommand p ts) id             
+ePropertySeq p ts = ePropertyUsing [eCommand (genInteractions ts) p] id             
 
 
-ePropertySeqCoverage :: MVar ExecEnv
-                     -> Chan [SolSignature]
+ePropertySeqCoverage :: [SolCall]
+                     -> MVar [CoverageInfo]
                      -> (VM -> Bool)
                      -> [SolSignature]
                      -> VM
                      -> Int
                      -> Property
-ePropertySeqCoverage execEnvRef goodInputs p ts v = ePropertyUsing (eCommandCoverage p ts) writeCoverage v where
-  writeCoverage :: MonadIO m => ReaderT CoverageRef (StateT VM m) a -> m a
-  writeCoverage m = do
-    threadCovRef <- liftIO $ newIORef mempty
-    let s = runReaderT m threadCovRef
-    a          <- evalStateT s v
-    threadCov  <- liftIO $ readIORef threadCovRef
-    execEnv    <- liftIO $ takeMVar execEnvRef
-    liftIO $ if isInteresting execEnv threadCov then writeChan goodInputs ts else return ()
-    liftIO $ putMVar execEnvRef (updateExecEnv execEnv threadCov)
-    return a
+ePropertySeqCoverage calls cov p ts v = ePropertyUsing (eCommandCoverage calls p ts) writeCoverage v
+  where
+    writeCoverage :: MonadIO m => ReaderT CoverageRef (StateT VM m) a -> m a
+    writeCoverage m = do
+      threadCovRef <- liftIO $ newIORef mempty
+      let s = runReaderT m threadCovRef
+      a            <- evalStateT s v
+      threadCov    <- liftIO $ readIORef threadCovRef
+      liftIO $ modifyMVar_ cov (pure . (:) threadCov)
+      return a
   
 
 -- Should work, but missing instance MonadBaseControl b m => MonadBaseControl b (PropertyT m)
