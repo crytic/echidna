6147831b0fb2258e0d5ceb089e2be57fa413dc41
Ben Perez <benperez1227@gmail.com>
2018-04-30 15:23:52 -0400

    Added functionality for taking the top performing tests from each round and feeding them into the next. Command line args now include number of epochs, will fix to be optional

--- src/Main.hs
+++ src/Main.hs
@@ -2,12 +2,10 @@
 
 module Main where
 
-import Control.Concurrent      (forkIO)
-import Control.Concurrent.MVar (takeMVar, newMVar)
-import Control.Concurrent.Chan (newChan)
+import Control.Concurrent.MVar (newMVar, readMVar, modifyMVar_)
+import Control.Monad           (forM_, forM)
 import Data.Maybe              (listToMaybe)
-import Data.Set                (size)
---import Data.MultiSet           (distinctSize)
+import Data.Set                (size, unions)
 import Data.Text               (pack)
 import System.Environment      (getArgs)
 
@@ -20,15 +18,24 @@ import Hedgehog.Internal.Property (GroupName(..), PropertyName(..))
 main :: IO ()
 main = getArgs >>= \case
   []  -> putStrLn "Please provide a solidity file to analyze"
-  filepath:args -> do
+  filepath:n:args -> do
     (v,a,ts)   <- loadSolidity filepath $ pack <$> listToMaybe args
-    execEnvRef <- newMVar $ ExecEnv {coverage = mempty, recent = [0,0,0,0,0,0], avg = 0}
-    goodInputs <- newChan
-    _          <- forkIO $ mutateGoodInputs goodInputs
-    let prop t = (PropertyName $ show t
-                 , ePropertySeqCoverage execEnvRef goodInputs (flip checkETest t) a v 10
-                 )
-    _ <- checkParallel . Group (GroupName filepath) $ map prop ts
-    l <- size . coverage <$> takeMVar execEnvRef
+    tests      <- mapM (\t -> return . ((,) t) =<< newMVar []) ts
+    let prop (cov,t,mvar) = (PropertyName $ show t
+                            , ePropertySeqCoverage cov mvar (flip checkETest t) a v 10
+                            )
+            
+    let epochs = read n :: Int
+    forM_ [1..epochs] $ \i -> do
+      putStrLn $ "Beginning Epoch " ++ show i
+      xs <- forM tests $ \(x,y) -> do
+        lastGen <- setCoverage <$> readMVar y
+        modifyMVar_ y (\_ -> pure [])
+        return (lastGen,x,y)
+
+      checkParallel . Group (GroupName filepath) $ map prop xs
+
+    ls <- mapM (readMVar . snd) tests
+    let l = size $ foldl (\acc xs -> unions (acc:(map snd xs))) mempty ls
     putStrLn $ "Coverage: " ++ show l ++ " unique PCs"
     return ()
