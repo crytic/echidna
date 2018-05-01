d00a6c69f9d2f62c3fb4cd311bcb7f39bb11506a
Ben Perez <benperez1227@gmail.com>
2018-05-01 16:31:37 -0400

    Coverage guidance now uses minimal set cover approx to generate inputs for each generation. Also added the option to turn off coverage guidance in the command line.

--- src/Main.hs
+++ src/Main.hs
@@ -2,7 +2,7 @@
 
 module Main where
 
-import Control.Concurrent.MVar (newMVar, readMVar, modifyMVar_)
+import Control.Concurrent.MVar (modifyMVar_, newMVar, readMVar)
 import Control.Monad           (forM_, forM)
 import Data.Maybe              (listToMaybe)
 import Data.Set                (size, unions)
@@ -18,18 +18,21 @@ import Hedgehog.Internal.Property (GroupName(..), PropertyName(..))
 main :: IO ()
 main = getArgs >>= \case
   []  -> putStrLn "Please provide a solidity file to analyze"
+
+  --RUN USING COVERAGE GUIDANCE
   filepath:n:args -> do
     (v,a,ts)   <- loadSolidity filepath $ pack <$> listToMaybe args
     tests      <- mapM (\t -> return . ((,) t) =<< newMVar []) ts
     let prop (cov,t,mvar) = (PropertyName $ show t
                             , ePropertySeqCoverage cov mvar (flip checkETest t) a v 10
                             )
-            
+
     let epochs = read n :: Int
     forM_ [1..epochs] $ \i -> do
       putStrLn $ "Beginning Epoch " ++ show i
       xs <- forM tests $ \(x,y) -> do
-        lastGen <- setCoverage <$> readMVar y
+        cov <- readMVar y
+        lastGen <- getCover cov
         modifyMVar_ y (\_ -> pure [])
         return (lastGen,x,y)
 
@@ -38,4 +41,15 @@ main = getArgs >>= \case
     ls <- mapM (readMVar . snd) tests
     let l = size $ foldl (\acc xs -> unions (acc:(map snd xs))) mempty ls
     putStrLn $ "Coverage: " ++ show l ++ " unique PCs"
+    return ()
+
+  --RUN WITHOUT COVERAGE GUIDANCE
+  filepath:args -> do
+    (v,a,ts) <- loadSolidity filepath $ pack <$> listToMaybe args
+
+    let prop t = (PropertyName $ show t
+                 , ePropertySeq (flip checkETest t) a v 10
+                 )
+
+    checkParallel . Group (GroupName filepath) $ map prop ts
     return ()
