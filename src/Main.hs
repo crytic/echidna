956223add03953ba55297a9535cbace3c49ed0cc
Ben Perez <benperez1227@gmail.com>
2018-05-02 18:37:59 -0400

    added optparse functionality, fixed comments in PR

--- src/Main.hs
+++ src/Main.hs
@@ -1,13 +1,13 @@
-{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE LambdaCase, OverloadedStrings, TupleSections #-}
 
 module Main where
 
-import Control.Concurrent.MVar (modifyMVar_, newMVar, readMVar)
+import Control.Concurrent.MVar (newMVar, readMVar, swapMVar)
 import Control.Monad           (forM_, forM)
-import Data.Maybe              (listToMaybe)
+import Data.List               (foldl')
 import Data.Set                (size, unions)
 import Data.Text               (pack)
-import System.Environment      (getArgs)
+import Data.Semigroup          ((<>))
 
 import Echidna.Exec
 import Echidna.Solidity
@@ -15,41 +15,70 @@ import Echidna.Solidity
 import Hedgehog hiding (checkParallel)
 import Hedgehog.Internal.Property (GroupName(..), PropertyName(..))
 
+import Options.Applicative
+
+
+data Options = Options
+  { filePath         :: FilePath
+  , selectedContract :: Maybe String
+  , solcArgs         :: Maybe String
+  , covEpochs        :: Maybe Int }
+
+options :: Parser Options
+options = Options
+      <$> argument str
+          ( metavar "FILE"
+         <> help "Solidity file to analyze" )
+      <*> optional ( argument str
+          ( metavar "CONTRACT"
+         <> help "Contract inside of file to analyze" ))
+      <*> optional ( strOption
+          ( long "solc-args"
+         <> help "Optional solidity compiler arguments" ))
+      <*> optional ( option auto
+          ( long "epochs"
+         <> help "Optional number of epochs to run coverage guidance" ))
+
+opts :: ParserInfo Options
+opts = info (options <**> helper)
+  ( fullDesc
+  <> progDesc "Fuzzing/property based testing of EVM code"
+  <> header "Echidna - Ethereum fuzz testing framework" )
+
+
 main :: IO ()
-main = getArgs >>= \case
-  []  -> putStrLn "Please provide a solidity file to analyze"
+main = do
+  (Options f c s n) <- execParser opts
+  (v,a,ts) <- loadSolidity f (pack <$> c) (pack <$> s)
 
-  --RUN USING COVERAGE GUIDANCE
-  filepath:n:args -> do
-    (v,a,ts)   <- loadSolidity filepath $ pack <$> listToMaybe args
-    tests      <- mapM (\t -> return . ((,) t) =<< newMVar []) ts
-    let prop (cov,t,mvar) = (PropertyName $ show t
-                            , ePropertySeqCoverage cov mvar (flip checkETest t) a v 10
-                            )
+  case n of
+    -- RUN WITHOUT COVERAGE
+    Nothing -> do
+      let prop t = (PropertyName $ show t
+                   , ePropertySeq (flip checkETest t) a v 10
+                   )
 
-    let epochs = read n :: Int
-    forM_ [1..epochs] $ \i -> do
-      putStrLn $ "Beginning Epoch " ++ show i
-      xs <- forM tests $ \(x,y) -> do
-        cov <- readMVar y
-        lastGen <- getCover cov
-        modifyMVar_ y (\_ -> pure [])
-        return (lastGen,x,y)
+      _ <- checkParallel . Group (GroupName f) $ map prop ts
+      return ()
 
-      checkParallel . Group (GroupName filepath) $ map prop xs
+    -- RUN WITH COVERAGE
+    Just epochs -> do
+      tests <- mapM (\t -> return . (t,) =<< newMVar []) ts
+      let prop (cov,t,mvar) = (PropertyName $ show t
+                              , ePropertySeqCoverage cov mvar (flip checkETest t) a v 10
+                              )
 
-    ls <- mapM (readMVar . snd) tests
-    let l = size $ foldl (\acc xs -> unions (acc:(map snd xs))) mempty ls
-    putStrLn $ "Coverage: " ++ show l ++ " unique PCs"
-    return ()
+      forM_ [1..epochs] $ \_ -> do
+        xs <- forM tests $ \(x,y) -> do
+          cov <- readMVar y
+          lastGen <- getCover cov
+          _ <- swapMVar y []
+          return (lastGen,x,y)
 
-  --RUN WITHOUT COVERAGE GUIDANCE
-  filepath:args -> do
-    (v,a,ts) <- loadSolidity filepath $ pack <$> listToMaybe args
+        checkParallel . Group (GroupName f) $ map prop xs
+        
+      ls <- mapM (readMVar . snd) tests
+      let l = size $ foldl' (\acc xs -> unions (acc:(map snd xs))) mempty ls
+      putStrLn $ "Coverage: " ++ show l ++ " unique PCs"
+      return ()
 
-    let prop t = (PropertyName $ show t
-                 , ePropertySeq (flip checkETest t) a v 10
-                 )
-
-    _ <- checkParallel . Group (GroupName filepath) $ map prop ts
-    return ()
