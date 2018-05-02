d1f8420a098d4666e5a443ef6d7ba902f91997e2
ggrieco-tob <31542053+ggrieco-tob@users.noreply.github.com>
2018-05-02 17:05:17 -0300

    Added ethereum owner address

--- lib/Echidna/Constants.hs
+++ lib/Echidna/Constants.hs
@@ -1,7 +1,8 @@
 module Echidna.Constants (
     ethRunAddress 
   , ethOtherAddress
-  , ethZeroAddress 
+  , ethZeroAddress
+  , ethOwnerAddress
 ) where 
 
 import Data.DoubleWord       (Word160(..))
@@ -11,6 +12,9 @@ ethRunAddress = 0xd30a286ec6737b8b2a6a7b5fbb5d75b895f62956
 
 ethOtherAddress :: Word160 
 ethOtherAddress = 0x67518339e369ab3d591d3569ab0a0d83b2ff5198
+
+ethOwnerAddress :: Word160
+ethOwnerAddress = 0x00a329c0648769a73afac7f9381e08fb43dbea72
 
 ethZeroAddress :: Word160 
 ethZeroAddress = 0x0 
