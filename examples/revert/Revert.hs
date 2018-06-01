c74bae01759fa6519a3811690394de967a54192b
Ben Perez <benperez1227@gmail.com>
2018-06-01 16:13:03 -0700

    Basic config file functionality

--- examples/revert/Revert.hs
+++ examples/revert/Revert.hs
@@ -1,18 +1,20 @@
 module Main where
 
 import Data.Text                  (Text)
+import Control.Monad.Reader       (runReader)
 import Control.Monad.State.Strict (evalState)
 import Hedgehog hiding            (checkParallel)
 import Hedgehog.Internal.Property (GroupName(..), PropertyName(..))
 
 import EVM (VM, VMResult(..), Error( Revert ))
 
+import Echidna.Config (defaultConfig)
 import Echidna.Exec
 import Echidna.Solidity
 
 main :: IO ()
 main = do (v,a,ts) <- loadSolidity "solidity/revert.sol" Nothing Nothing
-          let prop t = (PropertyName $ show t, ePropertySeq (`checkRTest` t) a v 10)
+          let prop t = (PropertyName $ show t, runReader (ePropertySeq (`checkRTest` t) a v) defaultConfig)
           _ <- checkParallel . Group (GroupName "revert.sol") $ map prop ts
           return ()
 
