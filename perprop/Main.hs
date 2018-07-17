c3ab51039d6c9782c7f5356c39a4264dcb3f9702
Ben Perez <benperez1227@gmail.com>
2018-07-17 15:03:10 -0400

    Made coverage always on

--- perprop/Main.hs
+++ perprop/Main.hs
@@ -3,23 +3,22 @@
 module Main where
 
 import Control.Concurrent.MVar (MVar, newMVar, readMVar, swapMVar)
-import Control.Lens hiding     (argument)
+import Control.Lens
 import Control.Monad           (forM, replicateM_)
 import Control.Monad.Identity  (Identity(..))
 import Control.Monad.Reader    (runReaderT)
-import Data.Semigroup          ((<>))
+import Data.List               (foldl')
+import Data.Set                (size, unions)
 import Data.Text               (Text)
 import Data.Yaml
 import EVM                     (VM)
 import EVM.Types               (Addr)
+import System.Environment     (getArgs)
 
 import qualified Data.ByteString as BS
 
 import Hedgehog hiding (checkParallel, Property)
 import Hedgehog.Internal.Property (GroupName(..), PropertyName(..))
-
-import Options.Applicative hiding (Parser,argument)
-import Options.Applicative as O
 
 import Echidna.ABI
 import Echidna.Config
@@ -27,33 +26,6 @@ import Echidna.Exec
 import Echidna.Property
 import Echidna.Solidity
 
-
--- Command line arguments parser
--- {{{
-
-data Options = Options
-  { filePath         :: FilePath
-  , configFilepath   :: FilePath
-  , coverageSelector :: Bool
-  }
-
-options :: O.Parser Options
-options = Options
-      <$> O.argument str
-          ( metavar "FILE"
-         <> help "Solidity file to analyze" )
-      <*> O.argument str
-          ( metavar "CONFIG"
-         <> help "Echidna config file" )
-      <*> switch
-          ( long "coverage"
-         <> help "Turn on coverage")
-
-opts :: ParserInfo Options
-opts = info (options <**> helper)
-  ( fullDesc
-  <> progDesc "Fuzzing/property based testing of EVM code"
-  <> header "Echidna - Ethereum fuzz testing framework" )
 
 -- }}}
 -- Types & instances
@@ -104,20 +76,13 @@ readConf f = decodeEither <$> BS.readFile f >>= \case
   Right (PerPropConf t s p) -> pure . Just . (,p) $
     defaultConfig & addrList .~ Just (view address <$> s) & testLimit .~ t
 
-group :: String -> Config -> [SolSignature] -> VM -> [Property] -> Group
-group n c a v p = Group (GroupName n) $ map prop p where
-  prop (Property f r) = ( PropertyName $ show f
-                        , useConfig (ePropertySeq (flip (checkTest r) f) a v))
-  -- typechecker gets mad if we don't explicitly destack here idk why
-  useConfig = runIdentity . (`runReaderT` c)
-
-groupWithCoverage :: String
-                  -> Config
-                  -> [SolSignature]
-                  -> VM
-                  -> [(Property,[SolCall],MVar [CoverageInfo])]
-                  -> Group
-groupWithCoverage n c a v ps = Group (GroupName n) $ map prop ps where
+group :: String
+      -> Config
+      -> [SolSignature]
+      -> VM
+      -> [(Property, [SolCall], MVar [CoverageInfo])]
+      -> Group
+group n c a v ps = Group (GroupName n) $ map prop ps where
   prop ((Property f r),cov,mvar) = ( PropertyName $ show f
                                    , useConfig (ePropertySeqCoverage cov mvar (flip (checkTest r) f) a v))
 
@@ -126,27 +91,27 @@ groupWithCoverage n c a v ps = Group (GroupName n) $ map prop ps where
 -- }}}
 -- Main
 -- {{{
-   
+
 main :: IO ()
-main = do
-  (Options file config useCov) <- execParser opts
-  readConf config >>= \case
-    Nothing -> pure ()
-    (Just (c,ps)) -> do
-      (v,a,_) <- runReaderT (loadSolidity file Nothing) c
-      if not useCov
-        then do
-        _ <- checkParallel $ group file c a v ps
-        pure ()
-        else do
-        tests <- mapM (\p -> fmap (p,) (newMVar [])) ps
-        replicateM_ (c ^. epochs) $ do
-          xs <- forM tests $ \(p,mvar) -> do
-            cov     <- readMVar mvar
-            lastGen <- getCover cov
-            _       <- swapMVar mvar []
-            Prelude.return (p,lastGen,mvar)
+main = getArgs >>= \case
+  [cf,sf] -> readConf cf >>= \case
+    Nothing       -> pure ()
+    (Just (c, ps)) -> do
+      (v,a,_) <- runReaderT (loadSolidity sf Nothing) c
+      tests <- mapM (\p -> fmap (p,) (newMVar [])) ps
+      replicateM_ (c ^. epochs) $ do
+        xs <- forM tests $ \(p,mvar) -> do
+          cov     <- readMVar mvar
+          lastGen <- getCover cov
+          _       <- swapMVar mvar []
+          Prelude.return (p,lastGen,mvar)
 
-          checkParallel $ groupWithCoverage file c a v xs
+        checkParallel $ group sf c a v xs
 
+      ls <- mapM (readMVar . snd) tests
+      let ci = foldl' (\acc xs -> unions (acc : map snd xs)) mempty ls
+      putStrLn $ "Coverage: " ++ show (size ci) ++ " unique arcs"
+
+  _ -> putStrLn "USAGE: ./perprop-exe config.yaml contract.sol"
+  
 -- }}}
