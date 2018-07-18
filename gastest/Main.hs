12da2d7a842ac349891bf654c973495b071fbfb8
JP Smith <jp@writes.co.de>
2018-07-18 18:07:25 -0400

    clean up code a bit

--- gastest/Main.hs
+++ gastest/Main.hs
@@ -2,7 +2,7 @@
 
 module Main where
 
-import Control.Lens ((^.))
+import Control.Lens (view)
 import Control.Monad (forM_, replicateM)
 import Control.Monad.IO.Class (liftIO)
 import Control.Monad.Reader (runReaderT)
@@ -23,15 +23,14 @@ main :: IO ()
 main = let conf m = runReaderT m defaultConfig in getArgs >>= \case
   []     -> putStrLn "need to specify solidity file"
   prog:_ -> do
-    (v,a,_) <- conf (loadSolidity prog Nothing)
+    (v,a,_) <- conf $ loadSolidity prog Nothing
     forM_ a $ \f -> do
       inputs <- liftIO $ replicateM 10000 . sample . conf $ genInteractions [f]
-      let results = map (\i -> (i, (v ^. state . gas) - (execState (execCall i) v ^. state . gas))) inputs
+      let results = map (\i -> (i, g v - g (execState (execCall i) v))) inputs; g = view $ state . gas
       mapM_ putStrLn [ unpack $ fst f
-                     , let m = maximumBy (comparing snd) results in
-                       "  Max: " ++ show (toInteger $ snd m) ++ " (" ++ displayAbiCall (fst m) ++ ")"
-                     , let m = minimumBy (comparing snd) results in
-                       "  Min: " ++ show (toInteger $ snd m) ++ " (" ++ displayAbiCall (fst m) ++ ")"
-                     , "  Avg: " ++ show (( fromIntegral $ sum (snd <$> results))
-                                          / fromIntegral (length results) :: Double)
-                     ]
+                     , pp maximumBy "Max" results, pp minimumBy "Min" results
+                     , "  Avg: " ++ show ( fromIntegral (sum $ snd <$> results)
+                                         / fromIntegral (length results) :: Double)
+                     ] where pp f s r = let (c, n) = f (comparing snd) r in
+                                            "  " ++ s ++ ": " ++ show (toInteger n)
+                                                 ++ " (" ++ displayAbiCall c ++ ")"
