ccafc093f586f83410dd5735858e5616d519b5a1
JP Smith <jp@writes.co.de>
2018-07-19 16:00:18 -0400

    try something even dumber

--- .travis/install-dapptools.sh
+++ .travis/install-dapptools.sh
@@ -6,6 +6,8 @@ git clone --recursive https://github.com/dapphub/dapptools/
 cd dapptools
 nix-env -f . -iA hevm
 cd src/hevm
+rm -rf ~/.cabal
+rm -rf ~/.ghc
 cabal install cabal cabal-install
 stack install
 cd ../../..
