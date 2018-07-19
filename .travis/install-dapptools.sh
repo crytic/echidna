db2dddd7b5befbcf9e372d14e2ad98a8071d3974
JP Smith <jp@writes.co.de>
2018-07-19 15:25:26 -0400

    not sure what's going on anymore

--- .travis/install-dapptools.sh
+++ .travis/install-dapptools.sh
@@ -2,11 +2,10 @@
 
 set -eux
 
-cabal install cabal cabal-install
 git clone --recursive https://github.com/dapphub/dapptools/
 cd dapptools
 nix-env -f . -iA hevm
 cd src/hevm
-stack solver
+cabal install cabal cabal-install
 stack install
 cd ../../..
