c881e2b81247637d31c00007c001dc375c3ec9d6
JP Smith <jp@writes.co.de>
2018-07-19 12:38:25 -0400

    use new dapptools build process

--- .travis/install-dapptools.sh
+++ .travis/install-dapptools.sh
@@ -2,9 +2,9 @@
 
 set -eux
 
-git clone https://github.com/dapphub/dapptools/
+git clone --recursive https://github.com/dapphub/dapptools/
 cd dapptools
-make install
+nix-env -f $HOME/.dapp/dapptools -iA hevm
 cd src/hevm
 stack solver
 stack install
