f2498b182edb702b6c1751d845d83bc83c0eaeee
ggrieco-tob <31542053+ggrieco-tob@users.noreply.github.com>
2018-07-23 14:05:46 -0300

    Updated build script to use a dapptools fork

--- .travis/install-dapptools.sh
+++ .travis/install-dapptools.sh
@@ -2,14 +2,15 @@
 
 set -eux
 
-git clone --recursive https://github.com/dapphub/dapptools/
+git clone https://github.com/ggrieco-tob/dapptools/
 cd dapptools
 #nix-env -f . -iA hevm
 cd src/libethjet
 sed -i "1s/.*/with import <nixpkgs> {};/" default.nix
 nix-build
+nix-env -i ./result
 cd ../../src/hevm
-# ugly hack
-printf 'cmVzb2x2ZXI6IGx0cy04LjE1CnBhY2thZ2VzOgotIC4KZXh0cmEtZGVwczoKLSBkYXRhLWR3b3JkLTAuMy4xCi0gZ2hjaS1wcmV0dHktMC4wLjIKLSBpcHByaW50LTAuNgotIHNyLWV4dHJhLTEuNDYuMy4yCi0gYnJpY2stMC4zNy4xCi0gbWVnYXBhcnNlYy02LjUuMAotIHJlc3RsZXNzLWdpdC0wLjUuMAotIHJvc2V6aXBwZXItMC4yCi0gcy1jYXJnb3QtMC4xLjQuMAotIHRyZWUtdmlldy0wLjUKLSBIU0gtMi4xLjMKLSBjb25maWctaW5pLTAuMi4yLjAKLSBkYXRhLWNsaXN0LTAuMS4yLjEKLSBwYXJzZXItY29tYmluYXRvcnMtMS4wLjAKLSB2dHktNS4yMQotIHdvcmQtd3JhcC0wLjQuMQoKZmxhZ3M6IHt9CmV4dHJhLXBhY2thZ2UtZGJzOiBbXQppbWFnZToKICBjb250YWluZXI6CiAgICBuYW1lOiBtYnJvY2svaGV2bQogICAgYmFzZTogaGFza2VsbDo4LjAuMQo=' | base64 -d > stack.yaml
-stack --nix --nix-packages libethjet install
+nix-env -i secp256k1
+ln -s ~/.nix-profile/ nix
+stack install
 cd ../../..
