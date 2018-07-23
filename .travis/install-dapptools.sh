89ab80c86948b61f54328788290c66c6dcd00d06
ggrieco-tob <31542053+ggrieco-tob@users.noreply.github.com>
2018-07-23 20:46:55 -0300

    Update install-dapptools.sh

--- .travis/install-dapptools.sh
+++ .travis/install-dapptools.sh
@@ -2,14 +2,9 @@
 
 set -eux
 
-git clone https://github.com/ggrieco-tob/dapptools/
+git clone https://github.com/dapphub/dapptools/
 cd dapptools
 cd src/libethjet
 sed -i "1s/.*/with import <nixpkgs> {};/" default.nix
-nix-build
-nix-env -i ./result
-cd ../../src/hevm
 nix-env -i secp256k1
-#ln -s ~/.nix-profile/ nix
-#LD_LIBRARY_PATH=nix/lib stack install
-cd ../../..
+nix-env -f . -i libethjet
