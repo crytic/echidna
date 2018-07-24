279c9cd9285a0ebcffd52b740fd9931e8cf618b7
ggrieco-tob <31542053+ggrieco-tob@users.noreply.github.com>
2018-07-24 10:48:21 -0300

    Update install-dapptools.sh

--- .travis/install-dapptools.sh
+++ .travis/install-dapptools.sh
@@ -5,6 +5,6 @@ set -eux
 git clone https://github.com/dapphub/dapptools/
 cd dapptools
 cd src/libethjet
-sed -i "s/{ stdenv, secp256k1 }:/with import <nixpkgs> {};/" default.nix
+sed -i.bak "s/{ stdenv, secp256k1 }:/with import <nixpkgs> {};/" default.nix
 nix-env -i secp256k1
 nix-env -f . -i libethjet
