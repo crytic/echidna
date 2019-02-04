b8b0d41e3ca592a0d91b46fbf0b60e5faab98529
ggrieco-tob <gustavo.grieco@trailofbits.com>
2019-02-04 16:21:36 -0500

    added a few contracts and reorganized examples

---
+++ examples/solidity/basic/killed.sol
@@ -0,0 +1,23 @@
+pragma solidity ^0.4.16;
+
+contract C {
+
+  uint private state = 0;
+  function f(uint x) public {
+    state = x;
+  }
+
+  function killed_if() {
+    require(state > 256);
+    suicide(0x0);
+  }
+
+  function echidna_still_alive() public returns (bool) {
+    return true;
+  }
+
+  function echidna_other_prop() public returns (bool) {
+    return true;
+  }
+
+}
---
+++ examples/solidity/rawtxs/interface.sol
@@ -0,0 +1,8 @@
+contract C {
+
+  function f(uint, int, int[]) { }
+  function g(bool, int, address[]) { }
+  function echidna_() returns (bool) {
+    return true;
+  }
+}
