74deb2a6f09820a95ced0e0d64ef74c1c35ba4f6
ggrieco-tob <gustavo.grieco@trailofbits.com>
2019-02-06 21:36:20 -0500

    added install-sol.sh script

---
+++ .travis/install-solc.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+# Adapted from https://github.com/commercialhaskell/stack
+
+set -eux
+
+mkdir -p ~/.local/bin;
+
+travis_retry() {
+  cmd=$*
+  $cmd || (sleep 2 && $cmd) || (sleep 10 && $cmd)
+}
+
+fetch_stack_osx() {
+}
+
+fetch_stack_linux() {
+  rm -Rf solc-static-linux;
+  curl https://github.com/ethereum/solidity/releases/download/v0.4.25/solc-static-linux;
+  chmod +x solc-static-linux;
+  mv solc-static-linux ~/.local/bin;
+}
+
+if [ "$(uname)" = "Darwin" ]; then
+  echo travis_retry fetch_stack_osx
+else
+  travis_retry fetch_stack_linux
+fi
+
+solc --version
