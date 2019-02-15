3e52bde1985f083fbd00e7a604aea2158fa40d6c
JP Smith <jp@writes.co.de>
2019-02-15 11:54:44 -0500

    clean up test file

--- src/test/Spec.hs
+++ src/test/Spec.hs
@@ -1,106 +1,64 @@
-{-# LANGUAGE FlexibleContexts #-}
-{-# LANGUAGE LambdaCase #-}
-
 import Test.Tasty
-import Test.Tasty.HUnit as HU
+import Test.Tasty.HUnit
 
+import Echidna.ABI (SolCall)
 import Echidna.Campaign (Campaign(..), tests, campaign, TestState(..))
-import Echidna.Config (EConfig, defaultConfig, sConf, parseConfig)
+import Echidna.Config (defaultConfig, parseConfig, sConf)
 import Echidna.Solidity (loadSolTests, quiet)
-import Echidna.Test (SolTest)
 import Echidna.Transaction (Tx, call)
 
-import Control.Lens ((^.), (&), (.~), view)
+import Control.Lens
 import Control.Monad.Reader (runReaderT)
-import Data.Maybe (fromJust)
-import Data.Text (Text)
+import Data.Maybe (isJust, maybe)
+import Data.Text (Text, unpack)
 import Data.List (find)
 import EVM.ABI (AbiValue(..))
+import System.Directory (withCurrentDirectory)
 
 main :: IO ()
-main = defaultMain spec
-
-spec :: TestTree
-spec = testGroup "Echidna" [solidityTests]
+main = withCurrentDirectory "./examples/solidity" . defaultMain $
+         testGroup "Echidna" [solidityTests]
 
 solidityTests :: TestTree
-solidityTests = testGroup "Solidity-HUnit"
-  [ testCase "Always True" $ testContract' c5 $
-      \c -> do
-        let tr = findtest' c "echidna_true"
-        assertBool "echidna_true somehow did not pass" $ passed tr
-  , testCase "Simple Flags Example" $ testContract' c2 $
-      \c -> do
-        let t1 = findtest' c "echidna_alwaystrue"
-            t2 = findtest' c "echidna_sometimesfalse"
-        assertBool "echidna_alwaystrue did not pass" $ passed t1
-        assertBool "echidna_sometimesfalse unsolved" $ solved t2
-        let sol = solve t2
-        assertBool "solution has length /= 2" $ length sol == 2
-  , testCase "Optimal Solve" $ testContract' c3 $
-      \c -> do
-        let tr = findtest' c "echidna_revert"
-        assertBool "echidna_revert unsolved" $ solved tr
-        let sol = solve tr
-        assertBool "solution has length /= 1" $ length sol == 1
-        let sol' = head sol
-        assertBool "solution is not f(-1)" $
-          case sol' ^. call of
-               Left ("f", [AbiInt _ (-1)]) -> True
-               _                           -> False
-  , testCase "Multisender" $ testContract c6 (Right cfg6) $
-      \c -> do
-        let tr = findtest' c "echidna_all_sender"
-        assertBool "echidna_all_sender unsolved" $ solved tr
-        let sol = solve tr
-        assertBool "solution has length /= 3" $ length sol == 3
-        let calls = view call <$> sol
-        assertBool "s1 not in solution" $ any (\case {Left ("s1", _) -> True; _ -> False;}) calls
-        assertBool "s2 not in solution" $ any (\case {Left ("s2", _) -> True; _ -> False;}) calls
-        assertBool "s3 not in solution" $ any (\case {Left ("s3", _) -> True; _ -> False;}) calls
+solidityTests = testGroup "Solidity Integration Testing"
+  [ testContract "basic/true.sol"        Nothing
+      [ ("echidna_true failed",                            not . solved "echidna_true") ]
+  , testContract "basic/flags.sol"       Nothing
+      [ ("echidna_alwaystrue failed",                      not . solved "echidna_alwaystrue")
+      , ("echidna_sometimesfalse passed",                  solved       "echidna_sometimesfalse")
+      , ("echidna_sometimesfalse didn't shrink optimally", solvedLen 2  "echidna_sometimesfalse")
+      ]
+  , testContract "basic/revert.sol"      Nothing
+      [ ("echidna_revert passed",                          solved      "echidna_revert")
+      , ("echidna_revert didn't shrink to length 1",       solvedLen 1 "echidna_revert")
+      , ("echidna_revert didn't shrink to f(-1)",
+         solvedWith ("f", [AbiInt 256 (-1)]) "echidna_revert")
+      ]
+  , testContract "basic/multisender.sol" (Just "basic/multisender.yaml") $
+      [ ("echidna_all_sender passed",                      solved             "echidna_all_sender")
+      , ("echidna_all_sender didn't shrink optimally",     solvedLen 3        "echidna_all_sender")
+      ] ++ (["s1", "s2", "s3"] <&> \n ->
+        ("echidna_all_sender solved without " ++ unpack n, solvedWith (n, []) "echidna_all_sender"))
   ]
-  where cd   = "./examples/solidity/basic/"
-        c2   = cd ++ "flags.sol"
-        c3   = cd ++ "revert.sol"
-        c5   = cd ++ "true.sol"
-        c6   = cd ++ "multisender.sol"
-        cfg6 = cd ++ "multisender.yaml"
 
-testContract :: FilePath -> Either EConfig FilePath -> (Campaign -> HU.Assertion) -> HU.Assertion
-testContract file cfg f = do
-  cfg' <- case cfg of
-               Left c   -> pure c
-               Right fp -> maybe (pure defaultConfig) parseConfig (Just fp)
-  let c = cfg' & sConf . quiet .~ True
-  results <- flip runReaderT c $ do
-    (v, w, ts) <- loadSolTests file Nothing
-    campaign (pure ()) v w ts
-  f results
+testContract :: FilePath -> Maybe FilePath -> [(String, Campaign -> Bool)] -> TestTree
+testContract fp cfg as = testCase fp $ do
+  c <- set (sConf . quiet) True <$> maybe (pure defaultConfig) parseConfig cfg
+  res <- runReaderT (loadSolTests fp Nothing >>= \(v, w, ts) -> campaign (pure ()) v w ts) c
+  mapM_ (\(t,f) -> assertBool t $ f res) as
 
-testContract' :: FilePath -> (Campaign -> HU.Assertion) -> HU.Assertion
-testContract' = flip testContract (Left defaultConfig)
+solnFor :: Text -> Campaign -> Maybe [Tx]
+solnFor t c = case fmap snd <$> find ((t ==) . fst . fst) $ c ^. tests of
+  Just (Large _ s) -> Just s
+  Just (Solved  s) -> Just s
+  _                -> Nothing
 
-solved :: TestState -> Bool
-solved (Large _ _) = True
-solved (Solved _)  = True
-solved _           = False
+solved :: Text -> Campaign -> Bool
+solved t = isJust . solnFor t
 
-passed :: TestState -> Bool
-passed Passed = True
-passed _      = False
+solvedLen :: Int -> Text -> Campaign -> Bool
+solvedLen i t = (== Just i) . fmap length . solnFor t
 
-solve :: TestState -> [Tx]
-solve (Large _ s) = s
-solve (Solved s) = s
-solve _ = undefined
-
-findtest :: Campaign -> Text -> Maybe TestState
-findtest = findtest'' . view tests
-
-findtest' :: Campaign -> Text -> TestState
-findtest' = (fromJust .) . findtest
-
-findtest'' :: [(SolTest, TestState)] -> Text -> Maybe TestState
-findtest'' xs t = case find ((t ==) . fst . fst) xs of
-                    Just (_,ts) -> Just ts
-                    Nothing     -> Nothing
+-- NOTE: this just verifies a call was found in the solution. Doesn't care about ordering/seq length
+solvedWith :: SolCall -> Text -> Campaign -> Bool
+solvedWith c t = maybe False (any $ (== Left c) . view call) . solnFor t
