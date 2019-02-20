edf1e0d1371b09b28bb65dd37ba6b7a7e014e710
JP Smith <jp@writes.co.de>
2019-02-20 11:21:24 -0500

    add regression tests

---
+++ examples/solidity/bad/notests.sol
@@ -0,0 +1,5 @@
+pragma solidity ^0.4.24;
+
+contract Abstract {
+  function f() pure public returns (int) {return 0;}
+}
---
+++ examples/solidity/bad/testargs.sol
@@ -0,0 +1,6 @@
+pragma solidity ^0.4.24;
+
+contract Abstract {
+  function f() pure public {}
+  function echidna_f(int x) pure public returns (int) {return 0;}
+}
