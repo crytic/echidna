ba76e02560301d55681c2e3e2d79427dd4ebd860
ggrieco-tob <gustavo.grieco@trailofbits.com>
2019-02-22 18:29:08 -0300

    improved ERC20 example

--- examples/solidity/tokens/bad_erc20.sol
+++ examples/solidity/tokens/bad_erc20.sol
@@ -1,12 +1,10 @@
-pragma solidity ^0.4.24;
-
 contract NewCoin {
   
   mapping(address => uint256) internal balances;
 
   uint256 internal totalSupply_;
 
-  function NewCoin() public {
+  constructor() public {
     totalSupply_ = 300;
   }
 
@@ -15,6 +13,7 @@ contract NewCoin {
   }
 
   function transfer(address _to, uint _value) public returns (bool) {
+ 
     require(_to != address(0));
     require(_value <= balances[msg.sender]);
 
@@ -28,11 +27,11 @@ contract NewCoin {
 
 contract TEST is NewCoin {
   uint private initSupply;
-  address private alice = 0x00a329c0648769a73afac7f9381e08fb43dbea50;
-  address private bob = 0x00a329c0648769a73afac7f9381e08fb43dbea60;
-  address private eve = 0x00a329c0648769a73afac7f9381e08fb43dbea70;
+  address private alice = 0x00A329C0648769a73aFAc7F9381E08fB43DbeA50;
+  address private bob = 0x00a329C0648769a73AFaC7f9381E08Fb43dBeA60;
+  address private eve = 0x00a329C0648769a73afAC7F9381e08fb43DBEA70;
 
-  function TEST() public {
+  constructor() public {
     balances[alice] = 100;
     balances[bob] = 100;
     balances[eve] = 100;
