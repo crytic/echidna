f269f9dd9c8d9407a248f6b6d0ff2d433400e2fe
JP Smith <jp@writes.co.de>
2019-02-27 18:20:57 -0500

    oops forgot to update contract

--- examples/solidity/basic/constants.sol
+++ examples/solidity/basic/constants.sol
@@ -7,6 +7,14 @@ contract Constants {
     if (i == 1337) {found = true;}
   }
 
+  function find2(address a) public {
+    if (a == 0x123) {found = true;}
+  }
+
+  function find3(string s) public {
+    if (keccak256(s) == keccak256("test")) {found = true;}
+  }
+
   function echidna_found() public view returns (bool) {
     return(!found);
   }
