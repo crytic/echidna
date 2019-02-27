229db2f721c92329a3b90aaf159a27e8e4604b47
Gustavo Grieco <31542053+ggrieco-tob@users.noreply.github.com>
2019-02-27 17:54:34 -0300

    Support for solc 0.5.x (#177)

--- examples/solidity/bad/onlytests.sol
+++ examples/solidity/bad/onlytests.sol
@@ -1,5 +1,3 @@
-pragma solidity ^0.4.24;
-
 contract Abstract {
   function echidna_f() pure public returns (int) {return 0;}
 }
--- examples/solidity/basic/multisender.sol
+++ examples/solidity/basic/multisender.sol
@@ -3,27 +3,27 @@ contract C {
   bool state2 = false;
   bool state3 = false;
   
-  function s1() {
+  function s1() public {
     //require(x == msg.sender);
-    require(msg.sender == 0x1);
+    require(msg.sender == address(0x1));
     state1 = true;
   }
 
 
-  function s2() {
+  function s2() public {
     //require(x == msg.sender);
-    require(msg.sender == 0x2);
+    require(msg.sender == address(0x2));
     state2 = true;
   }
 
 
-  function s3() {
+  function s3() public {
     //require(x == msg.sender);
-    require(msg.sender == 0x3);
+    require(msg.sender == address(0x3));
     state3 = true;
   }
 
-  function echidna_all_sender() returns (bool) {
+  function echidna_all_sender() public returns (bool) {
     return (!state1 || !state2 || !state3);
   }
 
