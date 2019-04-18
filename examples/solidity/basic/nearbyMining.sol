ef470915dfe249536c3b1de79b8e15150b1fde2b
agroce <agroce@gmail.com>
2019-04-18 09:59:05 -0700

    new tests

---
+++ examples/solidity/basic/nearbyMining.sol
@@ -0,0 +1,16 @@
+contract nearbyMining {
+  uint256 n = 0;
+  
+  function f (uint256 newn) public {
+    n = newn;
+  }
+  
+  function echidna_nearbyMining() public returns (bool) {
+    if (n <= 181888880989308019) {
+      return true;
+    }
+    if (n >= 181888880989308021) {
+      return true;
+    }
+  }
+}
