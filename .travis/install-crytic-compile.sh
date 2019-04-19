d8d012d4d7179150b6bc97c85934a4233cda5547
ggrieco-tob <gustavo.grieco@trailofbits.com>
2019-04-19 13:58:53 -0300

    fixed travis

--- .travis/install-crytic-compile.sh
+++ .travis/install-crytic-compile.sh
@@ -1,9 +1,4 @@
-sudo add-apt-repository ppa:jonathonf/python-3.6 -y
-sudo apt-get update
-sudo apt-get install python3.5 python3.6 python3-setuptools python3-apt -y
-#curl https://bootstrap.pypa.io/get-pip.py | python3.6 --user
-
 git clone https://github.com/crytic/crytic-compile --depth 1
 cd crytic-compile
-python3.6 setup.py install --user
+python setup.py install
 cd ..
