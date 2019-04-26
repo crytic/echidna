fda1c48a9811f6ac72898ea81af81963ccb07c51
ggrieco-tob <gustavo.grieco@trailofbits.com>
2019-04-26 10:42:36 -0300

    code climate fixes

--- lib/Echidna/Mutation.hs
+++ lib/Echidna/Mutation.hs
@@ -7,47 +7,47 @@ mutateBS :: MonadRandom m => Int -> ByteString -> m ByteString
 mutateBS n bs = do
                  f <- fromList [(identity, 1), (modify,3), (expand, 3), (delete, 3), (swap, 3)] 
                  xs <- f $ unpack bs 
-                 return $ pack $ (take n xs)
+                 return $ pack $ take n xs
 
                 where modify [] = return []
                       modify xs = do
-                                   i <- getRandomR (0, (length xs) - 1)
+                                   i <- getRandomR (0, length xs - 1)
                                    a <- getRandom
                                    return $ modify' xs i a
                       expand [] = return []
                       expand xs = do
-                                   i <- getRandomR (0, (length xs) - 1)
-                                   m <- getRandomR (0, (length xs) * 2) 
+                                   i <- getRandomR (0, length xs - 1)
+                                   m <- getRandomR (0, length xs * 2) 
                                    return $ expand' xs i m
                       delete [] = return []
                       delete xs = do
-                                   i <- getRandomR (0, (length xs) - 1)
-                                   return $ delete' xs i
+                                   i <- getRandomR (0, length xs - 1)
+                                   return $ delete' xs i 
                       swap [] = return []
                       swap xs = do
-                                   i <- getRandomR (0, (length xs) - 1)
-                                   j <- getRandomR (0, (length xs) - 1) 
+                                   i <- getRandomR (0, length xs - 1)
+                                   j <- getRandomR (0, length xs - 1) 
                                    return $ swap' xs i j
 
                       identity = return
 
 
 modify' :: [a] -> Int -> a -> [a]
-modify' []     _ _ = [] --error "empty list in modify'" 
+modify' []     _ _ = [] 
 modify' (x:xs) i a = case i of 
                       0   -> a:xs 
-                      _ -> x:(modify' xs (i-1) a)
+                      _ -> x: modify' xs (i-1) a
 
 
 expand' :: [a] -> Int -> Int -> [a]
-expand' []     _ _ = [] --error "empty list in expand'" 
+expand' []     _ _ = []
 expand' (x:xs) i n = case i of 
                        0   -> (take n $ repeat x) ++ xs 
-                       _ -> x:(expand' xs (i-1) n)
+                       _ -> x: expand' xs (i-1) n
 
 delete' :: [a] -> Int -> [a]
 delete' []  _ = []
-delete' xs  i = ys ++ (tail zs)
+delete' xs  i = ys ++ tail zs
                   where (ys,zs) = splitAt i xs  
 
 -- taken from https://stackoverflow.com/questions/30551033/swap-two-elements-in-a-list-by-its-indices/30551130#30551130
