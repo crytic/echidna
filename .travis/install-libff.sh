238fb0d7c9194feb56084d2651a88338da1b2aa6
ggrieco-tob <gustavo.grieco@trailofbits.com>
2019-06-03 18:07:45 -0300

    fix

--- .travis/install-libff.sh
+++ .travis/install-libff.sh
@@ -10,7 +10,8 @@ ARGS="-DWITH_PROCPS=OFF"
 if [ "$(uname)" == "Darwin" ]; then
   export LDFLAGS=-L/usr/local/opt/openssl/lib
   export CPPFLAGS=-I/usr/local/opt/openssl/include
+  export CXXFLAGS=-I/usr/local/opt/openssl/include
   ARGS="$ARGS -DOPENSSL_INCLUDE_DIR=/usr/local/opt/openssl/include/openssl"
 fi
-CXXFLAGS='-fPIC' cmake $ARGS .
+CXXFLAGS="-fPIC $CXXFLAGS" cmake $ARGS .
 sudo make install
