a088f835b008efbba9ebfe51e93af86e2154244d
ggrieco-tob <gustavo.grieco@trailofbits.com>
2019-06-03 17:19:13 -0300

    fix

--- .travis/install-libff.sh
+++ .travis/install-libff.sh
@@ -9,7 +9,7 @@ cd libff
 ARGS="-DWITH_PROCPS=OFF"
 if [ "$(uname)" == "Darwin" ]; then
   brew update # not sure if this is required
-  brew install openssl
+  brew upgrade openssl
   ARGS="$ARGS -DOPENSSL_INCLUDE_DIR=/usr/local/opt/openssl/include/openssl"
 fi
 CXXFLAGS='-fPIC' cmake $ARGS .
