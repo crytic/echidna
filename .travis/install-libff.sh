b00dc6232157598f4f63af585e2271602bcf39c6
JP Smith <jp@writes.co.de>
2019-06-05 14:39:06 -0400

    use bash single quotes properly

--- .travis/install-libff.sh
+++ .travis/install-libff.sh
@@ -12,7 +12,7 @@ if [ "$(uname)" == "Darwin" ]; then
   export LDFLAGS=-L/usr/local/opt/openssl/lib
   export CPPFLAGS=-I/usr/local/opt/openssl/include
   export CXXFLAGS=-I/usr/local/opt/openssl/include
-  ARGS="$ARGS -DOPENSSL_INCLUDE_DIR=/usr/local/opt/openssl/include/openssl -DCMAKE_CXX_STANDARD=14 -DCMAKE_CXX_FLAGS=\“-D_FILE_OFFSET_BITS=64 -DMIE_ATE_USE_GMP -DNDEBUG\""
+  ARGS='$ARGS -DOPENSSL_INCLUDE_DIR=/usr/local/opt/openssl/include/openssl -DCMAKE_CXX_STANDARD=14 -DCMAKE_CXX_FLAGS="-D_FILE_OFFSET_BITS=64 -DMIE_ATE_USE_GMP -DNDEBUG"'
 fi
 mkdir build
 cd build
