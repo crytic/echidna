77ca1f676a414fbe0d2ecc183aa77924b6ed0d5a
JP Smith <jp@writes.co.de>
2019-06-11 16:33:21 -0400

    set cxxflags to empty string for linux

--- .travis/install-libff.sh
+++ .travis/install-libff.sh
@@ -8,13 +8,14 @@ git clone https://github.com/scipr-lab/libff --recursive
 git submodule init && git submodule update
 cd libff
 ARGS="-DWITH_PROCPS=OFF"
+CXXFLAGS=""
 if [ "$TRAVIS_OS_NAME" = "osx" ]; then
-  export LDFLAGS=-L/usr/local/opt/openssl/lib;
-  export CPPFLAGS=-I/usr/local/opt/openssl/include;
-  export CXXFLAGS=-I/usr/local/opt/openssl/include;
-  ARGS="$ARGS -DOPENSSL_INCLUDE_DIR=/usr/local/opt/openssl/include/openssl -DCURVE=ALT_BN128";
-  sed -i '' 's/STATIC/SHARED/' libff/CMakeLists.txt; # Fix GHC segfaults from hell (idk why)
-  sed -i '' 's/STATIC/SHARED/' depends/CMakeLists.txt;
+  export LDFLAGS=-L/usr/local/opt/openssl/lib
+  export CPPFLAGS=-I/usr/local/opt/openssl/include
+  export CXXFLAGS=-I/usr/local/opt/openssl/include
+  ARGS="$ARGS -DOPENSSL_INCLUDE_DIR=/usr/local/opt/openssl/include/openssl -DCURVE=ALT_BN128"
+  sed -i '' 's/STATIC/SHARED/' libff/CMakeLists.txt # Fix GHC segfaults from hell (idk why)
+  sed -i '' 's/STATIC/SHARED/' depends/CMakeLists.txt
 fi
 mkdir build
 cd build
