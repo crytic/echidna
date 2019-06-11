80c7147e83b2e9307e90670b96467df2a91dac49
JP Smith <jp@writes.co.de>
2019-06-11 16:19:18 -0400

    fix linux build

--- .travis/install-libff.sh
+++ .travis/install-libff.sh
@@ -8,16 +8,15 @@ git clone https://github.com/scipr-lab/libff --recursive
 git submodule init && git submodule update
 cd libff
 ARGS="-DWITH_PROCPS=OFF"
-if [ "$(uname)" == "Darwin" ]; then
-  export LDFLAGS=-L/usr/local/opt/openssl/lib
-  export CPPFLAGS=-I/usr/local/opt/openssl/include
-  export CXXFLAGS=-I/usr/local/opt/openssl/include
-  ARGS="$ARGS -DOPENSSL_INCLUDE_DIR=/usr/local/opt/openssl/include/openssl -DCURVE=ALT_BN128"
-  sed -i '' 's/STATIC/SHARED/' libff/CMakeLists.txt # Fix GHC segfaults from hell (idk why)
-  sed -i '' 's/STATIC/SHARED/' depends/CMakeLists.txt
+if [ "$TRAVIS_OS_NAME" = "osx" ]; then
+  export LDFLAGS=-L/usr/local/opt/openssl/lib;
+  export CPPFLAGS=-I/usr/local/opt/openssl/include;
+  export CXXFLAGS=-I/usr/local/opt/openssl/include;
+  ARGS="$ARGS -DOPENSSL_INCLUDE_DIR=/usr/local/opt/openssl/include/openssl -DCURVE=ALT_BN128";
+  sed -i '' 's/STATIC/SHARED/' libff/CMakeLists.txt; # Fix GHC segfaults from hell (idk why)
+  sed -i '' 's/STATIC/SHARED/' depends/CMakeLists.txt;
 fi
 mkdir build
 cd build
 CXXFLAGS="-fPIC $CXXFLAGS" cmake $ARGS ..
 make && sudo make install
-ls /usr/local/lib
