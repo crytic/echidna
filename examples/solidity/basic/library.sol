bcd5b23054871c8596ebd0318ad07186b05e6837
ggrieco-tob <gustavo.grieco@trailofbits.com>
2019-06-12 13:24:52 -0300

    missing example

---
+++ examples/solidity/basic/library.sol
@@ -0,0 +1,23 @@
+library Test{
+    struct Storage{
+        bool flag;
+    }
+
+     function set(Storage storage st) public{
+        st.flag = true;
+    }
+
+ }
+
+contract Contract{
+    using Test for Test.Storage;
+    Test.Storage st;
+
+     function set() public{
+        st.set();
+    }
+
+     function echidna_library_call() external view returns (bool) {
+        return (!st.flag);
+    }
+}
