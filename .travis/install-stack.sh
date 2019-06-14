f9d01b9dfc22cb4a9ed58da8c587e10e4411fb3e
Gustavo Grieco <31542053+ggrieco-tob@users.noreply.github.com>
2019-06-14 15:01:39 -0300

    Fix stack installation in OSX using a previous release (#239)

--- .travis/install-stack.sh
+++ .travis/install-stack.sh
@@ -10,7 +10,7 @@ travis_retry() {
 }
 
 fetch_stack_osx() {
-  curl -skL https://www.stackage.org/stack/osx-x86_64 | tar xz --strip-components=1 --include '*/stack' -C ~/.local/bin;
+  curl -skL https://github.com/commercialhaskell/stack/releases/download/v1.9.3/stack-1.9.3-osx-x86_64.tar.gz | tar xz --strip-components=1 --include '*/stack' -C ~/.local/bin;
 }
 
 fetch_stack_linux() {
