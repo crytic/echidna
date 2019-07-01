ff0957719c06fe5c1c641e40d565b843f9dcd096
ggrieco-tob <gustavo.grieco@trailofbits.com>
2019-07-01 15:23:22 -0300

    crytic-compile ci test debugging

--- .travis/install-crytic-compile.sh
+++ .travis/install-crytic-compile.sh
@@ -1,8 +1,11 @@
 if [ "$(uname)" != "Darwin" ]; then
 
+    rm -Rf crytic-compile
     git clone https://github.com/crytic/crytic-compile --depth 1
     cd crytic-compile
     python setup.py install
+    echo $(which crytic-compile)
+    crytic-compile --help
     cd ..
 
 fi
