2fd34e8570d5a875a67c416add72b70766e6b305
JP Smith <jp@writes.co.de>
2019-07-11 12:18:12 -0400

    fix bad merge

--- src/test/Spec.hs
+++ src/test/Spec.hs
@@ -133,8 +133,8 @@ integrationTests = testGroup "Solidity Integration Testing"
       [ ("echidna_darray passed",                      solved             "echidna_darray")
       , ("echidna_darray didn't shrink optimally",     solvedLen 1        "echidna_darray") ]
   , testContract "basic/propGasLimit.sol" (Just "basic/propGasLimit.yaml") 
-      [ ("echidna_runForever passed",             solved "echidna_runForever") ]
-      [ ("echidna_darray passed",                  solved      "echidna_darray")
+      [ ("echidna_runForever passed",              solved      "echidna_runForever")
+      , ("echidna_darray passed",                  solved      "echidna_darray")
       , ("echidna_darray didn't shrink optimally", solvedLen 1 "echidna_darray") ]
   ]
 
