5263535265051f5d23e57e7ed50b549ab61cc781
Will Song <will.song@trailofbits.com>
2019-08-12 11:37:23 -0400

    parse abi json and add test abiv2 thing

---
+++ examples/solidity/abiv2/Ballot.sol
@@ -0,0 +1,24 @@
+pragma experimental ABIEncoderV2;
+pragma solidity ^0.4.25;
+
+contract Ballot {
+  struct Voter { // Struct
+      uint weight;
+      bool voted;
+      address delegate;
+      uint vote;
+  }
+
+  bool cond = true;
+
+  function add_voter(Voter memory voter) public returns (bool) {
+    if (voter.voted == true) {
+      cond = false;
+    }
+    return(true);
+  }
+
+  function echidna_test() public returns (bool) {
+    return(cond);
+  }
+}
