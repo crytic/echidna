b86708c419125579473a9a4b88c7c81dbc4eff1d
agroce <agroce@gmail.com>
2020-02-11 11:16:18 -0700

    add estimateGas config

--- lib/Echidna/Config.hs
+++ lib/Echidna/Config.hs
@@ -115,6 +115,7 @@ instance FromJSON EConfigWithUsage where
                                                   _         -> Nothing
                 cc = CampaignConf <$> v ..:? "testLimit"   ..!= 50000
                                   <*> v ..:? "stopOnFail"  ..!= False
+                                  <*> v ..:? "estimateGas" ..!= False
                                   <*> v ..:? "seqLen"      ..!= 100
                                   <*> v ..:? "shrinkLimit" ..!= 5000
                                   <*> cov
