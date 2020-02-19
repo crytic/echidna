78818e7ff513b74ac26f4ed23c05cc3433ed4e06
ggrieco-tob <gustavo.grieco@trailofbits.com>
2020-02-18 22:06:43 -0300

    fixes

--- lib/Echidna/Mutator.hs
+++ lib/Echidna/Mutator.hs
@@ -8,60 +8,15 @@ import EVM.ABI (AbiValue(..))
 
 mutateV :: MonadRandom m => Maybe Int -> [AbiValue] -> [AbiValue] -> m [AbiValue]
 mutateV mn fs vs = do
-  f <- fromList [(identity, 1), (insert, 3), (expand, 3), (delete, 3), (swap, 3)]
+  f <- fromList [(return, 1), (expandRandList, 3), (deleteRandList, 3), (swapRandList, 3)] 
   xs <- f vs
   return $ maybe xs (`take` (xs ++ fs)) mn
 
- where insert [] = return []
-       insert xs = do
-                    i <- getRandomR (0, length xs - 1)
-                    j <- getRandomR (0, length xs - 1)
-                    return $ insertAt (xs !! j) xs i
-       expand [] = return []
-       expand xs = do
-                    i <- getRandomR (0, length xs - 1)
-                    m <- getRandomR (1, length xs + 1) 
-                    return $ expandAt xs i m
-       delete [] = return []
-       delete xs = do
-                    i <- getRandomR (0, length xs - 1)
-                    return $ deleteAt i xs 
-       swap [] = return []
-       swap xs = do
-                   i <- getRandomR (0, length xs - 1)
-                   j <- getRandomR (0, length xs - 1) 
-                   return $ swapAt xs i j
-
-       identity = return
-
-
 mutateBS :: MonadRandom m => Maybe Int -> ByteString -> m ByteString
 mutateBS mn bs = do
-                 f <- fromList [(identity, 1), (insert, 3), (expand, 3), (delete, 3), (swap, 3)] 
-                 xs <- f $ unpack bs
-                 return $ pack $ maybe xs (`take` (xs ++ repeat 0)) mn
-
- where insert [] = return []
-       insert xs = do
-                    i <- getRandomR (0, length xs - 1)
-                    j <- getRandomR (0, length xs - 1)
-                    return $ insertAt (xs !! j) xs i
-       expand [] = return []
-       expand xs = do
-                    i <- getRandomR (0, length xs - 1)
-                    m <- getRandomR (1, length xs + 1) 
-                    return $ expandAt xs i m
-       delete [] = return []
-       delete xs = do
-                    i <- getRandomR (0, length xs - 1)
-                    return $ deleteAt i xs 
-       swap [] = return []
-       swap xs = do
-                   i <- getRandomR (0, length xs - 1)
-                   j <- getRandomR (0, length xs - 1) 
-                   return $ swapAt xs i j
-
-       identity = return
+  f <- fromList [(return, 1), (expandRandList, 3), (deleteRandList, 3), (swapRandList, 3)] 
+  xs <- f $ unpack bs
+  return $ pack $ maybe xs (`take` (xs ++ repeat 0)) mn
 
 
 replaceAt :: a -> [a] -> Int -> [a]
@@ -73,11 +28,21 @@ replaceAt a (x:xs) n =
     else x: replaceAt a xs (n-1)
 
 
+expandRandList :: MonadRandom m => [a] -> m [a]
+expandRandList []   = return []
+expandRandList rtxs = do k <- getRandomR (0, length rtxs - 1)
+                         return $ insertAt (rtxs !! k) rtxs k
+
 expandAt :: [a] -> Int -> Int -> [a]
 expandAt []     _ _ = []
 expandAt (x:xs) i n = case i of 
                        0   -> replicate n x ++ xs 
                        _   -> x: expandAt xs (i-1) n
+
+deleteRandList :: MonadRandom m => [a] -> m [a]
+deleteRandList []   = return []
+deleteRandList rtxs = do k <- getRandomR (0, length rtxs - 1)
+                         return $ deleteAt k rtxs
 
 deleteAt :: Int -> [a] -> [a]
 deleteAt _ [] = []
@@ -87,14 +52,20 @@ deleteAt i (a:as)
 
 insertAt :: a -> [a] -> Int -> [a]
 insertAt v [] _ = [v]
-insertAt v arr 1 = (v:arr)
-insertAt v (x:xs) n = (x:(insertAt v xs $ n - 1))
+insertAt v arr 1 = v : arr
+insertAt v (x:xs) n = x : insertAt v xs (n - 1)
 
-insertAtRandom :: MonadRandom m => [a] -> [a] -> m [a]
-insertAtRandom xs [] = return xs
-insertAtRandom xs (y:ys) = do idx <- getRandomR (0, (length xs) - 1) 
-                              insertAtRandom (insertAt y xs idx) ys
+insertRandList :: MonadRandom m => [a] -> [a] -> m [a]
+insertRandList xs [] = return xs
+insertRandList xs (y:ys) = do idx <- getRandomR (0, (length xs) - 1) 
+                              insertRandList (insertAt y xs idx) ys
 
+
+swapRandList :: MonadRandom m => [a] -> m [a]
+swapRandList []   = return []
+swapRandList rtxs = do k1 <- getRandomR (0, (length rtxs) - 1 )
+                       k2 <- getRandomR (0, (length rtxs) - 1 )
+                       return $ swapAt rtxs k1 k2
 
 -- taken from https://stackoverflow.com/questions/30551033/swap-two-elements-in-a-list-by-its-indices/30551130#30551130
 swapAt :: [a] -> Int -> Int -> [a]
@@ -108,7 +79,7 @@ swapAt xs i j =  let elemI = xs !! i
 spliceAtRandom :: MonadRandom m => [a] -> [a] -> m [a]
 spliceAtRandom xs1 xs2 = do idx1 <- getRandomR (0, (length xs1) - 1) 
                             idx2 <- getRandomR (0, (length xs2) - 1)
-                            return $ (take idx1 xs1) ++ (drop idx2 xs2)
+                            return $ take idx1 xs1 ++ drop idx2 xs2
 
 --applyAll :: [a -> a] -> a -> a
 --applyAll = appEndo . mconcat . map Endo
