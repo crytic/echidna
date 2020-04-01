d37af24a1880a6e74d6eec5d7e95b148183affa7
ggrieco-tob <gustavo.grieco@trailofbits.com>
2020-04-01 13:27:49 -0300

    fixed crytic-compile

--- .github/scripts/install-crytic-compile.sh
+++ .github/scripts/install-crytic-compile.sh
@@ -1,9 +1,3 @@
 if [ "$(uname)" != "Darwin" ]; then
-
-    rm -Rf crytic-compile
-    git clone https://github.com/crytic/crytic-compile --depth 1
-    cd crytic-compile
-    python setup.py install
-    cd ..
-
+    pip3 install crytic-compile --user
 fi
