f6b3a576a256de52a7be30810bacdbc88dd45d86
ggrieco-tob <gustavo.grieco@trailofbits.com>
2020-04-01 11:11:12 -0300

    fix

--- .github/scripts/install-libsecp256k1.sh
+++ .github/scripts/install-libsecp256k1.sh
@@ -2,10 +2,10 @@
 
 set -eux
 
-if [ -f $HOME/.local/lib/libsecp256k1.a ]; then
-  echo "libsecp256k1 exists, exiting..."
-  exit 0
-fi
+#if [ -f $HOME/.local/lib/libsecp256k1.a ]; then
+#  echo "libsecp256k1 exists, exiting..."
+#  exit 0
+#fi
 
 gitRef="1086fda4c1975d0cad8d3cad96794a64ec12dca4"
 curl -LO "https://github.com/bitcoin-core/secp256k1/archive/$gitRef.zip"
