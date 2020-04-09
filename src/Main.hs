313a4f8b3188e45040b54d2a6da4dd78cd31b5d7
Artur Cygan <artur.cygan@trailofbits.com>
2020-04-09 16:09:25 +0200

    JSON Output (#405)

--- src/Main.hs
+++ src/Main.hs
@@ -1,6 +1,8 @@
+{-# LANGUAGE RecordWildCards #-}
+
 module Main where
 
-import Control.Lens (view, (^.), to)
+import Control.Lens (view, (^.), to, (.~), (&))
 import Control.Monad (unless)
 import Control.Monad.Reader (runReaderT)
 import Control.Monad.Random (getRandom)
@@ -29,6 +31,7 @@ data Options = Options
   { filePath         :: NE.NonEmpty FilePath
   , selectedContract :: Maybe String
   , configFilepath   :: Maybe FilePath
+  , outputFormat     :: Maybe OutputFormat
   }
 
 options :: Parser Options
@@ -40,29 +43,43 @@ options = Options <$> (NE.fromList <$> some (argument str (metavar "FILES"
                   <*> optional (option str $ long "config"
                         <> metavar "CONFIG"
                         <> help "Config file")
+                  <*> optional (option auto $ long "format"
+                        <> metavar "FORMAT"
+                        <> help "Output format: json, text, none. Disables interactive UI")
 
 versionOption :: Parser (a -> a)
 versionOption = infoOption
                   ("Echidna " ++ showVersion version)
                   (long "version" <> help "Show version")
 
-opts :: ParserInfo Options
-opts = info (helper <*> versionOption <*> options) $ fullDesc
+optsParser :: ParserInfo Options
+optsParser = info (helper <*> versionOption <*> options) $ fullDesc
   <> progDesc "EVM property-based testing framework"
   <> header "Echidna"
 
 main :: IO ()
-main = do Options f c conf <- execParser opts
-          g   <- getRandom
-          EConfigWithUsage cfg ks _ <- maybe (pure (EConfigWithUsage defaultConfig mempty mempty)) parseConfig conf
-          unless (cfg ^. sConf . quiet) $ mapM_ (hPutStrLn stderr . ("Warning: unused option: " ++) . unpack) ks
-          let cd = corpusDir $ view cConf cfg
-          txs <- loadTxs cd
-          cpg <- flip runReaderT cfg $ do
-            cs       <- Echidna.Solidity.contracts f
-            ads      <- addresses
-            (v,w,ts) <- loadSpecified (pack <$> c) cs >>= prepareForTest
-            let ads' = AbiAddress <$> v ^. env . EVM.contracts . to keys
-            ui v w ts (Just $ mkGenDict (dictFreq $ view cConf cfg) (extractConstants cs ++ NE.toList ads ++ ads') [] g (returnTypes cs)) txs
-          saveTxs cd (map snd $ DS.toList $ view corpus cpg)
-          if not . isSuccess $ cpg then exitWith $ ExitFailure 1 else exitSuccess
+main = do
+  opts@Options{..} <- execParser optsParser
+  g <- getRandom
+  EConfigWithUsage loadedCfg ks _ <- maybe (pure (EConfigWithUsage defaultConfig mempty mempty)) parseConfig configFilepath
+  let cfg = overrideConfig loadedCfg opts
+  unless (cfg ^. sConf . quiet) $ mapM_ (hPutStrLn stderr . ("Warning: unused option: " ++) . unpack) ks
+  let cd = corpusDir $ view cConf cfg
+  txs <- loadTxs cd
+  cpg <- flip runReaderT cfg $ do
+    cs       <- Echidna.Solidity.contracts filePath
+    ads      <- addresses
+    (v,w,ts) <- loadSpecified (pack <$> selectedContract) cs >>= prepareForTest
+    let ads' = AbiAddress <$> v ^. env . EVM.contracts . to keys
+    ui v w ts (Just $ mkGenDict (dictFreq $ view cConf cfg) (extractConstants cs ++ NE.toList ads ++ ads') [] g (returnTypes cs)) txs
+  saveTxs cd (map snd $ DS.toList $ view corpus cpg)
+  if not . isSuccess $ cpg then exitWith $ ExitFailure 1 else exitSuccess
+  where
+  overrideConfig cfg Options{..} =
+    case maybe (cfg ^. uConf . operationMode) NonInteractive outputFormat of
+      Interactive -> cfg
+      NonInteractive Text ->
+        cfg & uConf . operationMode .~ NonInteractive Text
+      nonInteractive ->
+        cfg & sConf . quiet .~ True
+            & uConf . operationMode .~ nonInteractive
