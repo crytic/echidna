68b52890c7c7624f0fd7f89eb0acc74a1a9a7aef
ggrieco-tob <gustavo.grieco@trailofbits.com>
2020-11-06 09:02:19 -0300

    hlint fixes

--- lib/Echidna/Output/Source.hs
+++ lib/Echidna/Output/Source.hs
@@ -3,21 +3,20 @@
 {-# LANGUAGE PatternSynonyms #-}
 {-# LANGUAGE Rank2Types #-}
 {-# LANGUAGE ScopedTypeVariables #-}
-{-# LANGUAGE ViewPatterns #-}
 {-# LANGUAGE TupleSections #-}
 
 module Echidna.Output.Source where
 
-import Control.Lens hiding (index)
-import Data.Maybe (fromJust, mapMaybe)
+import Control.Lens
+import Data.Maybe (fromJust, fromMaybe, mapMaybe)
 import Data.Map.Strict (Map)
 import Data.Set (Set)
-import Data.Text (Text, append, index, length, unlines, pack, unpack)
+import Data.Text (Text, unlines, pack, unpack)
 import Data.Text.Encoding (decodeUtf8)
 import Data.Text.IO (writeFile)
 import Data.List (sort, nub)
 
-import EVM.Solidity --(SourceCache, SrcMap, SolcContract, contractName, sourceLines, sourceFiles, runtimeCode, runtimeSrcmap, creationSrcmap)
+import EVM.Solidity (stripBytecodeMetadata, SourceCache, SrcMap, SolcContract, sourceLines, sourceFiles, runtimeCode, runtimeSrcmap, creationSrcmap)
 import EVM.Debug (srcMapCodePos) --, srcMapCode)
 import Prelude hiding (unlines, writeFile)
 
@@ -43,15 +42,15 @@ saveCoveredCode Nothing  _  _  _ = pure ()
 -- | Pretty-print the covered code
 ppCoveredCode :: SourceCache -> [SolcContract] -> CoverageMap -> Text
 ppCoveredCode sc cs s | s == mempty = ""
-                      | otherwise   = unlines $ map snd $ concat $ map (\(f,vls) -> 
-                                                                           (mempty, (findFile f)) : 
-                                                                           (filterLines covLines $ map ((findFile f,) . decodeUtf8) $ V.toList vls)
+                      | otherwise   = unlines $ map snd $ concatMap (\(f,vls) -> 
+                                                                           (mempty, findFile f) : 
+                                                                           filterLines covLines (map ((findFile f,) . decodeUtf8) $ V.toList vls)
                                                                        ) allLines  
 --map (pack . show) $ S.toList $ allPositions (M.fromList $ map (\c -> (view contractName c,c)) cs) sc  --Lines
                                       where 
                                        allLines = M.toList $ view sourceLines sc
                                        findFile k = fst $ M.findWithDefault ("<no source code>", mempty) k (view sourceFiles sc)
-                                       covLines = concat $ map (srcMapCov sc s) cs 
+                                       covLines = concatMap (srcMapCov sc s) cs 
 
 markLine :: Int -> TxResult -> FilePathText -> [(FilePathText, Text)] -> [(FilePathText, Text)] 
 markLine n r cf ls = case splitAt (n-1) ls of
@@ -64,17 +63,17 @@ checkMarkers                        l  = pack $ ' ': ' ': ' ': ' ' : '|': l
 
 markStringLine  :: TxResult -> String -> Text
 markStringLine r (' ': ' ': ' ': ' ': '|':l) = pack $ getMarker r : ' ' : ' ' : ' ': '|' : l
-markStringLine r (m1: ' ': ' ': ' ':  '|':l)  = pack $ case (getMarker r) of
+markStringLine r (m1: ' ': ' ': ' ':  '|':l)  = pack $ case getMarker r of
                                                         m | m1 == m -> m1 : ' ' : ' ' : ' ': '|' : l
                                                         m           -> m1 :  m  : ' ' : ' ': '|' : l
 
-markStringLine r (m1: m2 : ' ': ' ': '|':l)  = pack $ case (getMarker r) of
+markStringLine r (m1: m2 : ' ': ' ': '|':l)  = pack $ case getMarker r of
                                                         m | m1 == m -> m1 :  m2  : ' ' : ' ': '|' : l
                                                         m | m2 == m -> m1 :  m2  : ' ' : ' ': '|' : l
                                                         m           -> m1 :  m2  : m   : ' ': '|' : l
 
 
-markStringLine r (m1: m2 : m3 : ' ': '|':l)  = pack $ case (getMarker r) of
+markStringLine r (m1: m2 : m3 : ' ': '|':l)  = pack $ case getMarker r of
                                                         m | m1 == m -> m1 :  m2  : m3 : ' ': '|' : l
                                                         m | m2 == m -> m1 :  m2  : m3 : ' ': '|' : l
                                                         m | m3 == m -> m1 :  m2  : m3 : ' ': '|' : l
@@ -90,9 +89,9 @@ getMarker ErrorOutOfGas = 'o'
 getMarker _             = 'e'
 
 filterLines :: [Maybe (FilePathText, Int, TxResult)] -> [(FilePathText, Text)] -> [(FilePathText, Text)]
-filterLines []                    ls  = ls
-filterLines (Nothing        : ns) ls  = filterLines ns ls
-filterLines ((Just (f,n,r)) : ns) ls  = filterLines ns (markLine n r f ls)
+filterLines []                  ls  = ls
+filterLines (Nothing      : ns) ls  = filterLines ns ls
+filterLines (Just (f,n,r) : ns) ls  = filterLines ns (markLine n r f ls)
 
 
 -- map (pack . show) $ S.toList $ allPositions (M.fromList $ map (\c -> (view contractName c,c)) cs) sc
@@ -111,9 +110,10 @@ allPositions solcByName sources =
 -}
 
 srcMapCov :: SourceCache -> Map BS.ByteString (Set (Int, TxResult)) -> SolcContract -> [Maybe (FilePathText, Int, TxResult)]
-srcMapCov sc s c = nub $ sort $ map (srcMapCodePos' sc) $ mapMaybe (srcMapForOpLocation c) $ S.toList $ maybe S.empty (id) $ M.lookup (stripBytecodeMetadata $ view runtimeCode c) s
+srcMapCov sc s c = nub $ sort $ map (srcMapCodePos' sc) $ mapMaybe (srcMapForOpLocation c) $ S.toList $ fromMaybe S.empty $ M.lookup (stripBytecodeMetadata $ view runtimeCode c) s
                    --catMaybes $ S.toList $ maybe S.empty (S.map (srcMapForOpLocation c . fst)) $ M.lookup (stripBytecodeMetadata $ view runtimeCode c) s
 
+srcMapCodePos' :: SourceCache -> (SrcMap, TxResult) -> Maybe (Text, Int, TxResult)
 
 srcMapCodePos' sc (n, r) = case srcMapCodePos sc n of 
                              Just (t,n') -> Just (t,n',r)
@@ -125,12 +125,9 @@ srcMapForOpLocation c (n,r) = case preview (ix n) (view runtimeSrcmap c <> view 
                                 _       -> Nothing
 
 linesByName :: SourceCache -> Map Text (V.Vector BS.ByteString)
-linesByName sources =
-      ( M.fromList
-      . map
+linesByName sources = M.fromList . map
           (\(k, v) ->
              (fst (fromJust (M.lookup k (view sourceFiles sources))), v))
       . M.toList
-      $ view sourceLines sources
-      ) 
+      $ view sourceLines sources 
 
