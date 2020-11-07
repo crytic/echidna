f6a61e74f194be2c822dcd74125058774f9aefb1
Gustavo Grieco <31542053+ggrieco-tob@users.noreply.github.com>
2020-11-07 10:29:23 -0300

    Update Research.hs

--- src/test/Tests/Research.hs
+++ src/test/Tests/Research.hs
@@ -10,7 +10,7 @@ researchTests = testGroup "Research-based Integration Testing"
       [ ("echidna_assert failed",     solved "echidna_assert") ]
   , testContract "research/harvey_baz.sol" Nothing
       [ ("echidna_all_states failed", solved "echidna_all_states") ]
-  , testContract "research/ilf_crowsale.sol" (Just ("research/ilf_crowsale.yaml"))
+  , testContract "research/ilf_crowsale.sol" (Just "research/ilf_crowsale.yaml")
       [ ("echidna_assert failed", solved "ASSERTION withdraw") ]
 
   ]
