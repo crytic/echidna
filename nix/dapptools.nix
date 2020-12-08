956ed7dfbb890510a83ab44b1765614886718b3b
Artur Cygan <arczicygan@gmail.com>
2020-12-08 16:51:55 +0100

    Fix and simplify Nix build

--- nix/dapptools.nix
+++
@@ -1,5 +0,0 @@
-let dapptools = builtins.fetchGit {
-      url = "https://github.com/dapphub/dapptools";
-      rev = "af84e2ee0a0654fdaa91186384233cf1731ee7ce";
-    };
-in import "${dapptools}" {}
