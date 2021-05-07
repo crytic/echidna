936e3d4b23ead94d287e745544f8567086086373
Gustavo Grieco <31542053+ggrieco-tob@users.noreply.github.com>
2021-05-07 11:27:59 -0300

    Echidna 1.7.1 (#663)

--- default.nix
+++ default.nix
@@ -32,7 +32,7 @@ let
     '';
   };
 
-  v = "1.7.0";
+  v = "1.7.1";
 
   f = { mkDerivation, aeson, ansi-terminal, base, base16-bytestring
       , binary, brick, bytestring, cborg, containers, data-dword, data-has
