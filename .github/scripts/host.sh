f0f20de47e41c051d52fc32d2dedc61590310579
Evan Sultanik <evan.sultanik@trailofbits.com>
2021-12-17 11:55:00 -0500

    MacOS Compatibility (#688)
    * Use homebrew to set the OpenSSL prefix
    
    * Use `uname` to set $HOST_OS if it is not set in the environment
    
    * Do not re-clone the libff repo if it already exists
    
    * Adds additional environment variables to appease OpenSSL/CMake

--- .github/scripts/host.sh
+++ .github/scripts/host.sh
@@ -4,6 +4,13 @@ if [ -z ${PREFIX+x} ]; then
   export PREFIX="$HOME/.local"
 fi
 
+if [ -z ${HOST_OS+x} ]; then
+  export HOST_OS=`uname`
+  if [ "$HOST_OS" = "Darwin" ]; then
+    export HOST_OS="macOS"
+  fi 
+fi
+
 if [ "$HOST_OS" = "Linux" ]; then
   EXT=".a"
 elif [ "$HOST_OS" = "macOS" ]; then
