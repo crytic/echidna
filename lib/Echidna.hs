46d20164254d26a029f55387331685e50b8c6be2
ggrieco-tob <gustavo.grieco@trailofbits.com>
2022-02-09 15:29:39 +0100

    code quality fix

--- lib/Echidna.hs
+++ lib/Echidna.hs
@@ -45,7 +45,8 @@ import qualified Data.List.NonEmpty as NE
 -- * A list of transaction sequences to initialize the corpus
 prepareContract :: (MonadCatch m, MonadRandom m, MonadReader x m, MonadIO m, MonadFail m,
                     Has TestConf x, Has TxConf x, Has SolConf x)
-                => EConfig -> NE.NonEmpty FilePath -> Maybe ContractName -> Seed -> m (VM, SourceCache, [SolcContract], World, [EchidnaTest], Maybe GenDict, [[Tx]])
+                => EConfig -> NE.NonEmpty FilePath -> Maybe ContractName -> Seed
+                -> m (VM, SourceCache, [SolcContract], World, [EchidnaTest], Maybe GenDict, [[Tx]])
 prepareContract cfg fs c g = do
   ctxs <- liftIO $ loadTxs cd
 
