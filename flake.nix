62331090d3271dd734c8326d96681dfac8cc304c
Artur Cygan <artur.cygan@trailofbits.com>
2022-07-28 15:08:35 +0200

    Add nix flake

---
+++ flake.nix
@@ -0,0 +1,17 @@
+{
+  inputs = {
+    nixpkgs.url = github:NixOS/nixpkgs;
+    newerNixpkgs.url = github:NixOS/nixpkgs;
+  };
+
+  outputs = { self, nixpkgs, newerNixpkgs }: {
+    defaultPackage.x86_64-linux = import ./. {
+      pkgs = nixpkgs.legacyPackages.x86_64-linux;
+      newerPkgs = newerNixpkgs.legacyPackages.x86_64-linux;
+    };
+    defaultPackage.x86_64-darwin = import ./. {
+      pkgs = nixpkgs.legacyPackages.x86_64-darwin;
+      newerPkgs = newerNixpkgs.legacyPackages.x86_64-darwin;
+    };
+  };
+}
