6009a6d431713ec4ed8e5d231e2d39cf73e0a96e
Artur Cygan <artur.cygan@trailofbits.com>
2022-07-28 16:02:20 +0200

    Add aarch64 to flake

--- flake.nix
+++ flake.nix
@@ -9,9 +9,17 @@
       pkgs = nixpkgs.legacyPackages.x86_64-linux;
       newerPkgs = newerNixpkgs.legacyPackages.x86_64-linux;
     };
+    defaultPackage.aarch64-linux = import ./. {
+      pkgs = nixpkgs.legacyPackages.aarch64-linux;
+      newerPkgs = newerNixpkgs.legacyPackages.aarch64-linux;
+    };
     defaultPackage.x86_64-darwin = import ./. {
       pkgs = nixpkgs.legacyPackages.x86_64-darwin;
       newerPkgs = newerNixpkgs.legacyPackages.x86_64-darwin;
+    };
+    defaultPackage.aarch64-darwin = import ./. {
+      pkgs = nixpkgs.legacyPackages.aarch64-darwin;
+      newerPkgs = newerNixpkgs.legacyPackages.aarch64-darwin;
     };
   };
 }
