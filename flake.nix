9d883338546444c7b21fb47ae44ede6302838973
Artur Cygan <artur.cygan@trailofbits.com>
2022-07-28 18:24:17 +0200

    Add cachix to flake

--- flake.nix
+++ flake.nix
@@ -1,4 +1,9 @@
 {
+  nixConfig = {
+    extra-substituters = "https://trailofbits.cachix.org";
+    extra-trusted-public-keys = "trailofbits.cachix.org-1:jRuxrlFghP6HstIaZg7DhvTgHyK/lcYa7U8y3CgKjzU=";
+  };
+
   inputs = {
     nixpkgs.url = github:NixOS/nixpkgs;
     newerNixpkgs.url = github:NixOS/nixpkgs;
