f3afd0909fd7b8d46976116dcb6eb71d1a4d8b3e
Artur Cygan <artur.cygan@trailofbits.com>
2022-11-22 00:37:27 +0100

    Move all nix stuff to flake, use nix-bundle-exe for macOS release

--- flake.nix
+++ flake.nix
@@ -5,21 +5,67 @@
   };
 
   inputs = {
-    nixpkgs.url = github:NixOS/nixpkgs;
+    nixpkgs.url = "github:NixOS/nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+    flake-compat = {
+      url = "github:edolstra/flake-compat";
+      flake = false;
+    };
+    nix-bundle-exe = {
+      url = "github:3noch/nix-bundle-exe";
+      flake = false;
+    };
   };
 
-  outputs = { self, nixpkgs }: {
-    defaultPackage.x86_64-linux = import ./. {
-      pkgs = nixpkgs.legacyPackages.x86_64-linux;
-    };
-    defaultPackage.aarch64-linux = import ./. {
-      pkgs = nixpkgs.legacyPackages.aarch64-linux;
-    };
-    defaultPackage.x86_64-darwin = import ./. {
-      pkgs = nixpkgs.legacyPackages.x86_64-darwin;
-    };
-    defaultPackage.aarch64-darwin = import ./. {
-      pkgs = nixpkgs.legacyPackages.aarch64-darwin;
-    };
-  };
+  outputs = { self, nixpkgs, flake-utils, nix-bundle-exe, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        # this is not perfect for development as it hardcodes solc to 0.5.7, test suite runs fine though
+        # would be great to integrate solc-select to be more flexible, improve this in future
+        solc = pkgs.stdenv.mkDerivation {
+          name = "solc";
+          src = if pkgs.stdenv.isDarwin then
+            pkgs.fetchurl {
+              url = "https://binaries.soliditylang.org/macosx-amd64/solc-macosx-amd64-v0.5.7+commit.6da8b019";
+              sha256 = "095mlw5x9lpdcdl9jzlvkvw46ag03xr4nj4vly4hgn92rgivimm7";
+            }
+          else
+            pkgs.fetchurl {
+              url = "https://binaries.soliditylang.org/linux-amd64/solc-linux-amd64-v0.5.7+commit.6da8b019";
+              sha256 = "0dsvzck5jh8rvdxs7zyn2ga9hif024msx8gr8ifgj4cmyb7m4341";
+            };
+          phases = ["installPhase" "patchPhase"];
+          installPhase = ''
+            mkdir -p $out/bin
+            cp $src $out/bin/solc
+            chmod +x $out/bin/solc
+          '';
+        };
+        echidna = with pkgs; lib.pipe
+          (haskellPackages.callCabal2nix "echidna" ./. {})
+          [
+            (haskell.lib.compose.addTestToolDepends [ haskellPackages.hpack slither-analyzer solc ])
+          ];
+      in rec {
+        packages.echidna = echidna;
+        packages.default = echidna;
+
+        packages.echidna-bundle =
+          pkgs.callPackage nix-bundle-exe {} (pkgs.haskell.lib.dontCheck echidna);
+
+        devShell = with pkgs;
+          haskellPackages.shellFor {
+            packages = _: [ echidna ];
+            shellHook = "hpack";
+            buildInputs = [
+              solc
+              haskellPackages.hlint
+              haskellPackages.cabal-install
+              haskellPackages.haskell-language-server
+            ];
+            withHoogle = true;
+          };
+      }
+    );
 }
