509cb65c0a7b7d4e5d62b071428bd7f197563316
Artur Cygan <artur.cygan@trailofbits.com>
2022-11-22 20:07:51 +0100

    Cleanup redundant type constraints (#853)

--- lib/Echidna/Fetch.hs
+++ lib/Echidna/Fetch.hs
@@ -3,12 +3,10 @@ module Echidna.Fetch where
 import Control.Lens
 import Control.Monad.Catch (MonadThrow(..), throwM)
 import Control.Monad.IO.Class (MonadIO(..))
-import Control.Monad.Reader (MonadReader)
 import Control.Monad.State.Strict (execStateT)
 import Data.ByteString (ByteString, pack, append)
 import Data.ByteString.Base16 qualified as BS16 (decode)
 import Data.Either (fromRight)
-import Data.Has (Has(..))
 import Data.Text (Text)
 import Data.Text.Encoding (encodeUtf8)
 
@@ -16,12 +14,12 @@ import EVM
 import EVM.Solidity
 import EVM.Types (Addr)
 
-import Echidna.Types.Solidity (SolConf(..), SolException(..))
+import Echidna.Types.Solidity (SolException(..))
 import Echidna.Types.Tx (createTx, unlimitedGasPerBlock)
 import Echidna.Exec (execTx)
 
 -- | Deploy a list of solidity contracts in certain addresses
-deployBytecodes' :: (MonadIO m, MonadThrow m, MonadReader x m, Has SolConf x)
+deployBytecodes' :: (MonadIO m, MonadThrow m)
                        => [(Addr, ByteString)] -> Addr -> VM -> m VM
 deployBytecodes' []          _ vm = return vm
 deployBytecodes' ((a, bc):cs) d vm = deployBytecodes' cs d =<< loadRest
@@ -31,10 +29,10 @@ deployBytecodes' ((a, bc):cs) d vm = deployBytecodes' cs d =<< loadRest
                        (Just (VMSuccess _)) -> return vm'
                        _                    -> throwM $ DeploymentFailed a
 
-deployContracts :: (MonadIO m, MonadThrow m, MonadReader x m, Has SolConf x)
+deployContracts :: (MonadIO m, MonadThrow m)
                        => [(Addr, SolcContract)] -> Addr -> VM -> m VM
 deployContracts cs = deployBytecodes' $ map (\(a, c) -> (a, c ^. creationCode)) cs
 
-deployBytecodes :: (MonadIO m, MonadThrow m, MonadReader x m, Has SolConf x)
+deployBytecodes :: (MonadIO m, MonadThrow m)
                        => [(Addr, Text)] -> Addr -> VM -> m VM
 deployBytecodes cs = deployBytecodes' $ map (\(a, bc) -> (a, fromRight (error ("invalid b16 decoding of: " ++ show bc)) $ BS16.decode $ encodeUtf8 bc)) cs
