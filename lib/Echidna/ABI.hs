361bfd5f1a5a96c3c47ba4c9bc56a1f506460a62
Emilio López <emilio.lopez@trailofbits.com>
2023-01-10 12:09:31 -0300

    Use `bit` instead of power of two (2 ^ i)
    We are computing powers of two, so using `bit` performs better.

--- lib/Echidna/ABI.hs
+++ lib/Echidna/ABI.hs
@@ -9,6 +9,7 @@ import Control.Monad (join, liftM2, liftM3, foldM, replicateM)
 import Control.Monad.Random.Strict (MonadRandom, getRandom, getRandoms, getRandomR, uniformMay)
 import Control.Monad.Random.Strict qualified as R
 import Data.Binary.Put (runPut, putWord32be)
+import Data.Bits (bit)
 import Data.Bool (bool)
 import Data.ByteString.Lazy as BSLazy (toStrict)
 import Data.ByteString (ByteString)
@@ -49,10 +50,10 @@ commonTypeSizes :: [Int]
 commonTypeSizes = [8,16..256]
 
 mkValidAbiInt :: Int -> Int256 -> Maybe AbiValue
-mkValidAbiInt i x = if abs x <= 2 ^ (i - 1) - 1 then Just $ AbiInt i x else Nothing
+mkValidAbiInt i x = if abs x < bit (i - 1) then Just $ AbiInt i x else Nothing
 
 mkValidAbiUInt :: Int -> Word256 -> Maybe AbiValue
-mkValidAbiUInt i x = if x <= 2 ^ i - 1 then Just $ AbiUInt i x else Nothing
+mkValidAbiUInt i x = if x < bit i then Just $ AbiUInt i x else Nothing
 
 makeNumAbiValues :: Integer -> [AbiValue]
 makeNumAbiValues i = let l f = f <$> commonTypeSizes <*> fmap fromIntegral ([i-1..i+1] ++ [(-i)-1 .. (-i)+1]) in
