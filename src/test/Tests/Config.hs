8a1e0c998c9d7ef107b5f45828b52d8dd4483827
Artur Cygan <artur.cygan@trailofbits.com>
2023-02-21 17:23:24 +0100

    Fix W256 values parsing from config (#935)

--- src/test/Tests/Config.hs
+++ src/test/Tests/Config.hs
@@ -1,14 +1,16 @@
 module Tests.Config (configTests) where
 
 import Test.Tasty (TestTree, testGroup)
-import Test.Tasty.HUnit (testCase, assertBool, (@?=))
+import Test.Tasty.HUnit (testCase, assertBool, (@?=), assertFailure)
 
 import Control.Lens (sans)
 import Control.Monad (void)
 import Data.Function ((&))
+import Data.Yaml qualified as Y
 
 import Echidna.Types.Config (EConfigWithUsage(..), EConfig(..))
 import Echidna.Types.Campaign (CampaignConf(..))
+import Echidna.Types.Tx (TxConf(..))
 import Echidna.Config (defaultConfig, parseConfig)
 
 configTests :: TestTree
@@ -24,6 +26,16 @@ configTests = testGroup "Configuration tests" $
       assertBool ("unused options: " ++ show bad) $ null bad
       let unset' = unset & sans "seed"
       assertBool ("unset options: " ++ show unset') $ null unset'
+  , testCase "W256 decoding" $ do
+      let maxW256  = "0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff"
+          overW256 = "0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff0"
+      case Y.decodeEither' ("maxGasprice: " <> maxW256) of
+        Right (c :: EConfigWithUsage) | c.econfig.txConf.maxGasprice == maxBound -> pure ()
+        Right _ -> assertFailure "wrong value decoded"
+        Left e -> assertFailure $ "unexpected decoding error: " <> show e
+      case Y.decodeEither' ("maxGasprice: " <> overW256) of
+        Right (_ :: EConfigWithUsage) -> assertFailure "should not decode"
+        Left _ -> pure ()
   ]
   where files = ["basic/config.yaml", "basic/default.yaml"]
         assertCoverage config value = config.campaignConf.knownCoverage @?= value
