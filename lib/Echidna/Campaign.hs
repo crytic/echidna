05c41c328f775023c3d4d2604ec86c4c07b2e6fa
Artur Cygan <artur.cygan@trailofbits.com>
2023-02-22 23:51:33 +0100

    Friendlier names

--- lib/Echidna/Campaign.hs
+++ lib/Echidna/Campaign.hs
@@ -8,8 +8,7 @@ import Control.Lens
 import Control.Monad (replicateM, when, unless)
 import Control.Monad.Catch (MonadCatch(..), MonadThrow(..))
 import Control.Monad.Random.Strict (MonadRandom, RandT, evalRandT)
-import Control.Monad.Reader.Class (MonadReader)
-import Control.Monad.Reader (runReaderT, asks, liftIO)
+import Control.Monad.Reader (MonadReader, asks, liftIO)
 import Control.Monad.State.Strict (MonadState(..), StateT(..), evalStateT, execStateT, gets, MonadIO)
 import Control.Monad.Trans (lift)
 import Control.Monad.Trans.Random.Strict (liftCatch)
@@ -41,7 +40,6 @@ import Echidna.Types (Gas)
 import Echidna.Types.Buffer (forceBuf)
 import Echidna.Types.Campaign
 import Echidna.Types.Config
-import Echidna.Types.Corpus (InitialCorpus)
 import Echidna.Types.Coverage (coveragePoints)
 import Echidna.Types.Signature (makeBytecodeCache, MetadataCache)
 import Echidna.Types.Test
@@ -125,20 +123,21 @@ evalSeq vmForShrink e = go [] where
 -- gas usage for each call
 updateGasInfo :: [(Tx, (VMResult, Gas))] -> [Tx] -> Map Text (Gas, [Tx]) -> Map Text (Gas, [Tx])
 updateGasInfo [] _ gi = gi
-updateGasInfo ((t@(Tx (SolCall (f, _)) _ _ _ _ _ _), (_, used')):ts) tseq gi =
+updateGasInfo ((tx@(Tx (SolCall (f, _)) _ _ _ _ _ _), (_, used')):txs) tseq gi =
   case mused of
     Nothing -> rec
     Just (used, _) | used' > used -> rec
     Just (used, otseq) | (used' == used) && (length otseq > length tseq') -> rec
-    _ -> updateGasInfo ts tseq' gi
+    _ -> updateGasInfo txs tseq' gi
   where mused = Map.lookup f gi
-        tseq' = t:tseq
-        rec   = updateGasInfo ts tseq' (Map.insert f (used', reverse tseq') gi)
+        tseq' = tx:tseq
+        rec   = updateGasInfo txs tseq' (Map.insert f (used', reverse tseq') gi)
 updateGasInfo ((t, _):ts) tseq gi = updateGasInfo ts (t:tseq) gi
 
 -- | Execute a transaction, capturing the PC and codehash of each instruction executed, saving the
 -- transaction if it finds new coverage.
-execTxOptC :: (MonadIO m, MonadReader Env m, MonadState (VM, Campaign) m, MonadThrow m) => Tx -> m (VMResult, Gas)
+execTxOptC :: (MonadIO m, MonadReader Env m, MonadState (VM, Campaign) m, MonadThrow m)
+           => Tx -> m (VMResult, Gas)
 execTxOptC tx = do
   (vm, Campaign{_coverage = oldCov}) <- get
   let cov = _2 . coverage
@@ -162,64 +161,64 @@ addToCorpus :: MonadState Campaign m => Int -> [(Tx, (VMResult, Gas))] -> m ()
 addToCorpus n res = unless (null rtxs) $ corpus %= Set.insert (n, rtxs)
   where rtxs = fst <$> res
 
--- | Generate a new sequences of transactions, either using the corpus or with randomly created transactions
+-- | Generate a new sequences of transactions, either using the corpus or with
+-- randomly created transactions
 randseq :: (MonadRandom m, MonadReader Env m, MonadState Campaign m)
-        => MetadataCache -> InitialCorpus -> Int -> Map Addr Contract -> World -> m [Tx]
-randseq memo (n,txs) ql o w = do
-  ca <- get
-  cs <- asks (.cfg.campaignConf.mutConsts)
+        => MetadataCache -> Int -> Map Addr Contract -> World -> m [Tx]
+randseq memo seqLen deployedContracts world = do
+  camp <- get
+  mutConsts <- asks (.cfg.campaignConf.mutConsts)
   txConf <- asks (.cfg.txConf)
-  let ctxs = ca._corpus
-      -- TODO: include reproducer when optimizing
-      --rs   = filter (not . null) $ map (view testReproducer) $ ca ^. tests
-      p    = ca._ncallseqs
-  if n > p then -- Replay the transactions in the corpus, if we are executing the first iterations
-    return $ txs !! p
-  else do
-    -- Randomly generate new random transactions
-    gtxs <- replicateM ql $ runReaderT (genTxM memo o) (w, txConf)
-    -- Generate a random mutator
-    cmut <- if ql == 1 then seqMutatorsStateless (fromConsts cs) else seqMutatorsStateful (fromConsts cs)
-    -- Fetch the mutator
-    let mut = getCorpusMutation cmut
-    if Set.null ctxs then
-      return gtxs      -- Use the generated random transactions
-    else
-      mut ql ctxs gtxs -- Apply the mutator
+  -- TODO: include reproducer when optimizing
+  --let rs = filter (not . null) $ map (.testReproducer) $ ca._tests
+  -- Generate new random transactions
+  randTxs <- replicateM seqLen (genTx memo world txConf deployedContracts)
+  -- Generate a random mutator
+  cmut <- if seqLen == 1 then seqMutatorsStateless (fromConsts mutConsts)
+                         else seqMutatorsStateful (fromConsts mutConsts)
+  -- Fetch the mutator
+  let mut = getCorpusMutation cmut
+  if null camp._corpus
+    then pure randTxs -- Use the generated random transactions
+    else mut seqLen camp._corpus randTxs -- Apply the mutator
 
--- | Given an initial 'VM' and 'World' state and a number of calls to generate, generate that many calls,
--- constantly checking if we've solved any tests or can shrink known solves. Update coverage as a result
+-- | Given an initial 'VM' and 'World' state and a number of calls to generate,
+-- generate that many calls, constantly checking if we've solved any tests or
+-- can shrink known solves. Update coverage as a result
 callseq :: (MonadIO m, MonadCatch m, MonadRandom m, MonadReader Env m, MonadState Campaign m)
-        => InitialCorpus -> VM -> World -> Int -> m ()
-callseq ic v w ql = do
+        => [[Tx]] -> VM -> World -> Int -> m ()
+callseq initialCorpus vm world seqLen = do
   conf <- asks (.cfg.campaignConf)
-  -- First, we figure out whether we need to execute with or without coverage optimization and gas info,
-  -- and pick our execution function appropriately
+  -- First, we figure out whether we need to execute with or without coverage
+  -- optimization and gas info, and pick our execution function appropriately
   let coverageEnabled = isJust conf.knownCoverage
-  let ef = if coverageEnabled then execTxOptC else (\t -> do (xd, ca) <- get
-                                                             (r, vm') <- runStateT (execTx t) xd
-                                                             put (vm', ca)
-                                                             pure r)
-      old = v._env._contracts
-  let gasEnabled = conf.estimateGas
+  let ef = if coverageEnabled
+              then execTxOptC
+              else \tx -> do (v, ca) <- get
+                             (r, vm') <- runStateT (execTx tx) v
+                             put (vm', ca)
+                             pure r
   -- Then, we get the current campaign state
   ca <- get
   -- Then, we generate the actual transaction in the sequence
   metaCacheRef <- asks (.metadataCache)
   metaCache <- liftIO $ readIORef metaCacheRef
-  is <- randseq metaCache ic ql old w
+  txSeq <-
+    -- Replay transactions in the corpus during the first iterations
+    if length initialCorpus > ca._ncallseqs
+      then pure $ initialCorpus !! ca._ncallseqs
+      else randseq metaCache seqLen vm._env._contracts world
   -- We then run each call sequentially. This gives us the result of each call, plus a new state
-  (res, (vm, camp)) <- runStateT (evalSeq v ef is) (v, ca)
-  let new = vm._env._contracts
-      -- compute the addresses not present in the old VM via set difference
-      diff = Map.keys $ new \\ old
+  (res, (vm', camp)) <- runStateT (evalSeq vm ef txSeq) (vm, ca)
+  let -- compute the addresses not present in the old VM via set difference
+      diff = Map.keys $ vm'._env._contracts \\ vm._env._contracts
       -- and construct a set to union to the constants table
       diffs = H.fromList [(AbiAddressType, Set.fromList $ AbiAddress <$> diff)]
   -- Save the global campaign state (also vm state, but that gets reset before it's used)
   put camp -- Update the gas estimation
-  when gasEnabled $ gasInfo %= updateGasInfo res []
+  when conf.estimateGas $ gasInfo %= updateGasInfo res []
   -- If there is new coverage, add the transaction list to the corpus
-  when (camp._newCoverage) $ addToCorpus (camp._ncallseqs + 1) res
+  when camp._newCoverage $ addToCorpus (camp._ncallseqs + 1) res
   -- Reset the new coverage flag
   newCoverage .= False
   -- Keep track of the number of calls to `callseq`
@@ -262,7 +261,7 @@ campaign
   -> Maybe GenDict       -- ^ Optional generation dictionary
   -> [[Tx]]              -- ^ Initial corpus of transactions
   -> m Campaign
-campaign u vm w ts d txs = do
+campaign u vm world ts dict initialCorpus = do
   conf <- asks (.cfg.campaignConf)
 
   metaCacheRef <- asks (.metadataCache)
@@ -271,26 +270,25 @@ campaign u vm w ts d txs = do
   liftIO $ writeIORef metaCacheRef (memo (vm._env._contracts <> external))
 
   let c = fromMaybe mempty conf.knownCoverage
-  let effectiveSeed = fromMaybe d'.defSeed conf.seed
-      effectiveGenDict = d' { defSeed = effectiveSeed }
-      d' = fromMaybe defaultDict d
+  let effectiveSeed = fromMaybe dict'.defSeed conf.seed
+      effectiveGenDict = dict' { defSeed = effectiveSeed }
+      dict' = fromMaybe defaultDict dict
       camp = Campaign ts c mempty effectiveGenDict False Set.empty 0
   execStateT (evalRandT (lift u >> runCampaign) (mkStdGen effectiveSeed)) camp
   where
     memo = makeBytecodeCache . map (forceBuf . (^. bytecode)) . Map.elems
     runCampaign = gets (fmap (.testState) . (._tests)) >>= update
-    update c    = do
-      let ic = (length txs, txs)
+    update c = do
       CampaignConf{testLimit, stopOnFail, seqLen, shrinkLimit} <- asks (.cfg.campaignConf)
       Campaign{_ncallseqs} <- get
       if | stopOnFail && any (\case Solved -> True; Failed _ -> True; _ -> False) c ->
            lift u
          | any (\case Open  n   -> n <= testLimit; _ -> False) c ->
-           callseq ic vm w seqLen >> step
+           callseq initialCorpus vm world seqLen >> step
          | any (\case Large n   -> n < shrinkLimit; _ -> False) c ->
            step
          | null c && (seqLen * _ncallseqs) <= testLimit ->
-           callseq ic vm w seqLen >> step
+           callseq initialCorpus vm world seqLen >> step
          | otherwise ->
            lift u
     step = runUpdate (shrinkTest vm) >> lift u >> runCampaign
