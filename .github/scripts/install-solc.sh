baf05c669ebea1c2176554d8688d3471ef3eeb06
Emilio López <emilio.lopez@trailofbits.com>
2023-02-22 20:05:53 -0300

    Run tests on Windows as well

--- .github/scripts/install-solc.sh
+++ .github/scripts/install-solc.sh
@@ -6,35 +6,64 @@ set -eux
 
 mkdir -p $HOME/.local/bin;
 
+wget() {
+  if [ -f '/c/msys64/usr/bin/wget.exe' ]; then
+    cmd="/c/msys64/usr/bin/wget.exe"
+  else
+    cmd="wget"
+  fi
+
+  command "$cmd" -q $*
+}
+
 travis_retry() {
   cmd=$*
   $cmd || (sleep 2 && $cmd) || (sleep 10 && $cmd)
 }
 
-fetch_solc_linux() {
+fetch_solc() {
   VER="$1"
-  if [ ! -f "$HOME/.local/bin/solc-$VER" ]; then
-    rm -Rf solc-static-linux
-    wget "https://github.com/ethereum/solidity/releases/download/v$VER/solc-static-linux"
-    chmod +x solc-static-linux
-    mv solc-static-linux "$HOME/.local/bin/solc-$VER"
+  FILE="$2"
+  EXT="$3"
+
+  if [ ! -f "$HOME/.local/bin/solc-$VER$EXT" ]; then
+    rm -Rf "$FILE"
+    wget "https://github.com/ethereum/solidity/releases/download/v$VER/$FILE"
+    chmod +x "$FILE"
+    mv "$FILE" "$HOME/.local/bin/solc-$VER$EXT"
     echo "Downloaded solc $VER"
   else
     echo "Skipped solc $VER, already present"
   fi
 }
 
-fetch_all_solc_linux() {
-  fetch_solc_linux "0.4.25"
-  fetch_solc_linux "0.5.7"
-  fetch_solc_linux "0.6.12"
-  fetch_solc_linux "0.7.5"
+fetch_solc_linux() {
+  fetch_solc "$1" "solc-static-linux" ""
+}
+
+fetch_solc_windows() {
+  fetch_solc "$1" "solc-windows.exe" ".exe"
+}
+
+fetch_all() {
+  FETCHER="$1"
+
+  "$FETCHER" "0.4.25"
+  "$FETCHER" "0.5.7"
+  "$FETCHER" "0.6.12"
+  "$FETCHER" "0.7.5"
 }
 
 if [ "$HOST_OS" = "Linux" ]; then
   if [ "${SOLC_VER:-}" == "" ]; then
-    travis_retry fetch_all_solc_linux
+    travis_retry fetch_all fetch_solc_linux
   else
     travis_retry fetch_solc_linux "$SOLC_VER"
+  fi
+elif [ "$HOST_OS" = "Windows" ]; then
+  if [ "${SOLC_VER:-}" == "" ]; then
+    travis_retry fetch_all fetch_solc_windows
+  else
+    travis_retry fetch_solc_windows "$SOLC_VER"
   fi
 fi
