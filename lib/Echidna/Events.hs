6dd61979bd8e1f88c1f7f0b90e08d27cd017d788
Artur Cygan <artur.cygan@trailofbits.com>
2023-03-02 20:47:31 -0400

    Show human readable errors for panic codes (#965)

--- lib/Echidna/Events.hs
+++ lib/Echidna/Events.hs
@@ -5,76 +5,106 @@ module Echidna.Events where
 
 import Data.ByteString qualified as BS
 import Data.ByteString.Lazy (fromStrict)
+import Data.Map (Map)
+import Data.Map qualified as Map
+import Data.Maybe (fromJust, catMaybes, maybeToList)
+import Data.Text (pack, Text)
 import Data.Tree (flatten)
 import Data.Tree.Zipper (fromForest, TreePos, Empty)
-import Data.Text (pack, Text)
-import Data.Map qualified as M
-import Data.Maybe (listToMaybe, fromJust)
 import Data.Vector (fromList)
 
-import EVM
-import EVM.ABI (Event(..), Indexed(..), decodeAbiValue, AbiType(AbiUIntType, AbiTupleType, AbiStringType))
+import EVM hiding (code)
+import EVM.ABI (Event(..), Indexed(..), decodeAbiValue, AbiType(..), AbiValue(..))
 import EVM.Dapp (DappContext(..), DappInfo(..))
 import EVM.Format (showValues, showError, contractNamePart)
 import EVM.Solidity (SolcContract(..))
 import EVM.Types (Expr(ConcreteBuf), W256, maybeLitWord)
 
-import Echidna.Types.Buffer (forceLit)
-import qualified Data.Map as Map
+import Echidna.Types.Buffer (forceLit, forceBuf)
+import Data.ByteString (ByteString)
 
-type EventMap = M.Map W256 Event
+type EventMap = Map W256 Event
 type Events = [Text]
 
 emptyEvents :: TreePos Empty a
 emptyEvents = fromForest []
 
-maybeContractNameFromCodeHash :: (?context :: DappContext) => EVM.Types.W256 -> Maybe Text
-maybeContractNameFromCodeHash codeHash = fmap contractToName maybeContract
-  where maybeContract = snd <$> Map.lookup codeHash ?context.info.solcByHash
-        contractToName c = contractNamePart c.contractName
-
 extractEvents :: Bool -> DappInfo -> VM -> Events
 extractEvents decodeErrors dappInfo vm =
-  let eventMap = dappInfo.eventMap
-      forest = traceForest vm
-      showTrace trace =
-        let ?context = DappContext { info = dappInfo, env = vm._env._contracts } in
-        let codehash' = fromJust $ maybeLitWord trace._traceContract._codehash
-            maybeContractName = maybeContractNameFromCodeHash codehash'
-        in
-        case trace._traceData of
-          EventTrace addr bytes topics ->
-            case maybeLitWord =<< listToMaybe topics of
-              Nothing   -> []
-              Just word -> case M.lookup word eventMap of
-                             Just (Event name _ types) ->
-                               -- TODO this is where indexed types are filtered out
-                               -- they are filtered out for a reason as they only contain
-                               -- the topic hash which is printed super verbose by dapptools
-                               [name <>
-                                showValues [t | (_, t, NotIndexed) <- types] bytes <>
-                                pack " from: " <>
-                                maybe mempty (\ x -> x <> pack "@") maybeContractName <>
-                                pack (show $ forceLit addr)]
-                             Nothing -> [pack $ show word]
-          ErrorTrace e ->
-            case e of
-              Revert out -> ["merror " <> "Revert " <> showError out <> maybe mempty (\ x -> pack " from: " <> x) maybeContractName]
-              _ -> ["merror " <> pack (show e)]
+  let forest = traceForest vm
+  in maybeToList (decodeRevert decodeErrors vm)
+     ++ catMaybes (concatMap flatten (fmap (fmap showTrace) forest))
+  where
+  showTrace trace =
+    let ?context = DappContext { info = dappInfo, env = vm._env._contracts } in
+    let codehash' = fromJust $ maybeLitWord trace._traceContract._codehash
+        maybeContractName = maybeContractNameFromCodeHash dappInfo codehash'
+    in case trace._traceData of
+      EventTrace addr bytes (topic:_) ->
+        case Map.lookup (forceLit topic) dappInfo.eventMap of
+          Just (Event name _ types) ->
+            -- TODO this is where indexed types are filtered out
+            -- they are filtered out for a reason as they only contain
+            -- the topic hash which is printed super verbose by dapptools
+            Just $
+              name
+              <> showValues [t | (_, t, NotIndexed) <- types] bytes
+              <> " from: "
+              <> maybe mempty (<> "@") maybeContractName
+              <> pack (show $ forceLit addr)
+          Nothing -> Just $ pack $ show (forceLit topic)
+      ErrorTrace e ->
+        case e of
+          Revert out ->
+            Just $
+              "error Revert "
+              <> showError out
+              <> maybe "" (": " <>) (maybeHumanPanic (forceBuf out))
+              <> maybe "" (" from: " <>) maybeContractName
+          _ ->
+            Just $ "error " <> pack (show e)
+      _ -> Nothing
 
-          _ -> []
-  in decodeRevert decodeErrors vm ++ concat (concatMap flatten $ fmap (fmap showTrace) forest)
+  maybeHumanPanic bs =
+    case BS.splitAt 4 bs of
+      ("\x4e\x48\x7b\x71", d) ->
+        Just $ humanPanic $ decodePanic d
+      _ -> Nothing
 
+maybeContractNameFromCodeHash :: DappInfo -> W256 -> Maybe Text
+maybeContractNameFromCodeHash info codeHash = fmap contractToName maybeContract
+  where maybeContract = snd <$> Map.lookup codeHash info.solcByHash
+        contractToName c = contractNamePart c.contractName
 
-decodeRevert :: Bool -> VM -> Events
+decodeRevert :: Bool -> VM -> Maybe Text
 decodeRevert decodeErrors vm =
   case vm._result of
     Just (VMFailure (Revert (ConcreteBuf bs))) -> decodeRevertMsg decodeErrors bs
-    _                            -> []
+    _ -> Nothing
 
-decodeRevertMsg :: Bool -> BS.ByteString -> Events
+decodeRevertMsg :: Bool -> ByteString -> Maybe Text
 decodeRevertMsg decodeErrors bs =
   case BS.splitAt 4 bs of
-    ("\x08\xc3\x79\xa0",d) | decodeErrors -> ["Error" <> (pack . show $ decodeAbiValue (AbiTupleType (fromList [AbiStringType])) (fromStrict d))]
-    ("\x4e\x48\x7b\x71",d)                -> ["Panic(" <> (pack . show $ decodeAbiValue (AbiUIntType 256) (fromStrict d)) <> ")"]
-    _                                     -> []
+    ("\x08\xc3\x79\xa0", d) | decodeErrors ->
+      Just $ "Error" <> (pack . show $ decodeAbiValue (AbiTupleType (fromList [AbiStringType])) (fromStrict d))
+    ("\x4e\x48\x7b\x71", d) ->
+      Just $ "Panic(" <> (pack . show $ decodePanic d) <> "): " <> humanPanic (decodePanic d)
+    _ -> Nothing
+
+decodePanic :: ByteString -> AbiValue
+decodePanic v = decodeAbiValue (AbiUIntType 256) (fromStrict v)
+
+humanPanic :: AbiValue -> Text
+humanPanic (AbiUInt _ code) =
+  case code of
+    0x01 -> "Using assert."
+    0x11 -> "SafeMath over-/under-flows."
+    0x12 -> "Divide by 0."
+    0x21 -> "Conversion into non-existent enum type."
+    0x22 -> "Incorrectly encoded storage byte array."
+    0x31 -> "pop() on an empty array."
+    0x32 -> "Index out of bounds exception."
+    0x41 -> "Allocating too much memory or creating a too large array."
+    0x51 -> "Calling a zero-initialized variable of internal function type."
+    _ -> "Unknown panic error code."
+humanPanic _ = error "Shouldn't happen, improve types to make this branch go away"
