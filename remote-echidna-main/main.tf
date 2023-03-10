ab1e1fecfa04334499055b2e780ea3bf9242ad75
Antonio Guilherme Ferreira Viggiano <agfviggiano@gmail.com>
2023-03-10 20:10:13 -0300

    remove

--- remote-echidna-main/main.tf
+++
@@ -1,4 +0,0 @@
-provider "aws" {
-  region = var.region
-}
-
--- remote-echidna-main/output.tf
+++
@@ -1,7 +0,0 @@
-output "ec2_instance_public_ip" {
-  value = aws_instance.ec2_instance.public_ip
-}
-
-output "s3_bucket_name" {
-  value = aws_s3_bucket.s3_bucket.id
-}
