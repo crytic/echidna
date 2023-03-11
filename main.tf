cdb434c1c233ffa1a2e8481e3757534ee8e16eb5
Antonio Guilherme Ferreira Viggiano <agfviggiano@gmail.com>
2023-03-10 23:56:34 -0300

    Update main

--- main.tf
+++ main.tf
@@ -1,3 +1,9 @@
 module "remote-echidna" {
   source = "github.com/aviggiano/remote-echidna"
+
+  project               = var.project
+  project_git_url       = var.project_git_url
+  run_tests_cmd         = var.run_tests_cmd
+  ec2_instance_key_name = var.ec2_instance_key_name
+  project_git_checkout  = var.project_git_checkout
 }
