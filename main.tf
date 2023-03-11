520ce3757618476d978e4f23ce27d9c135a41884
Antonio Guilherme Ferreira Viggiano <agfviggiano@gmail.com>
2023-03-10 23:58:55 -0300

    Add vars

--- main.tf
+++ main.tf
@@ -7,3 +7,29 @@ module "remote-echidna" {
   ec2_instance_key_name = var.ec2_instance_key_name
   project_git_checkout  = var.project_git_checkout
 }
+
+
+variable "project" {
+  type        = string
+  description = "Your project name"
+}
+
+variable "ec2_instance_key_name" {
+  type        = string
+  description = "EC2 instance key name. Needs to be manually created first on the AWS console"
+}
+
+variable "project_git_url" {
+  type        = string
+  description = "Project Git URL"
+}
+
+variable "project_git_checkout" {
+  type        = string
+  description = "Project Git checkout (branch or commit hash)"
+}
+
+variable "run_tests_cmd" {
+  type        = string
+  description = "Command to run echidna tests"
+}
