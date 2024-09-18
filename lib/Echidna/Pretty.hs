4ee7099bd2285c795e5054bdeb91644062bc0071
Emilio López <2642849+elopez@users.noreply.github.com>
2024-09-18 11:25:14 +0200

    Add labels to call sequence (#1314)
    * Add labels to call sequence
    
    hevm already supports the `vm.label(address, string)` cheatcode, but
    Echidna has not made use of the labels so far in its output. This adds
    some basic support when printing call sequences. When available, it
    will show the corresponding label next to any from/to address, or
    next to any address arguments.
    
    * Fix hlint warnings

--- lib/Echidna/Pretty.hs
+++ lib/Echidna/Pretty.hs
@@ -3,21 +3,24 @@ module Echidna.Pretty where
 import Data.ByteString.Base16 qualified as BS16
 import Data.ByteString.Char8 qualified as BSC8
 import Data.List (intercalate)
-import Data.Text (unpack)
+import Data.Map (Map)
+import Data.Text (Text, unpack)
+
+import EVM.Types (Addr)
 
 import Echidna.ABI (ppAbiValue)
 import Echidna.Types.Signature (SolCall)
 import Echidna.Types.Tx (TxCall(..))
 
 -- | Pretty-print some 'AbiCall'.
-ppSolCall :: SolCall -> String
-ppSolCall (t, vs) =
+ppSolCall :: Map Addr Text -> SolCall -> String
+ppSolCall labels (t, vs) =
   (if t == "" then unpack "*fallback*" else unpack t)
-  ++ "(" ++ intercalate "," (ppAbiValue <$> vs) ++ ")"
+  ++ "(" ++ intercalate "," (ppAbiValue labels <$> vs) ++ ")"
 
 -- | Pretty-print some 'TxCall'
-ppTxCall :: TxCall -> String
-ppTxCall (SolCreate _)    = "<CREATE>"
-ppTxCall (SolCall x)      = ppSolCall x
-ppTxCall NoCall           = "*wait*"
-ppTxCall (SolCalldata x)  = BSC8.unpack $ "0x" <> BS16.encode x
+ppTxCall :: Map Addr Text -> TxCall -> String
+ppTxCall _ (SolCreate _)    = "<CREATE>"
+ppTxCall labels (SolCall x) = ppSolCall labels x
+ppTxCall _ NoCall           = "*wait*"
+ppTxCall _ (SolCalldata x)  = BSC8.unpack $ "0x" <> BS16.encode x
