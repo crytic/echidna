16729d28ac91ba734e365c5e542b9067a327655f
figtracer <1gusredo@gmail.com>
2025-03-28 23:58:15 +0000

    updated integration.hs per review

--- src/test/Tests/Integration.hs
+++ src/test/Tests/Integration.hs
@@ -104,4 +104,6 @@ integrationTests = testGroup "Solidity Integration Testing"
       [ ("test passed",                    solved     "test") ]
   , testContract "basic/flags.sol" (Just "basic/etheno-query-error.yaml")
       [] -- Just check if the etheno config does not crash Echidna
+  , testContractV "tstore/tstore.sol" (Just (>= solcV (0,8,25))) Nothing
+      [ ("echidna_foo passed", solved "echidna_foo") ]
   ]
