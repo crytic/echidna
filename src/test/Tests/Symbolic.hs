1bdd291437522dc61aaeb9a8e8479d4edb271d88
Emilio López <emilio.lopez@trailofbits.com>
2025-08-15 18:52:18 -0300

    tests: test verify also with bitwuzla

--- src/test/Tests/Symbolic.hs
+++ src/test/Tests/Symbolic.hs
@@ -5,18 +5,21 @@ import Common (testContract', solcV, solved, verified)
 import Echidna.Types.Worker (WorkerType(..))
 
 symbolicTests :: TestTree
-symbolicTests = testGroup "Symbolic tests"
-  [ testContract' "symbolic/verify.sol" Nothing (Just (>= solcV (0,6,9))) (Just "symbolic/verify.yaml") True SymbolicWorker
+symbolicTests = testGroup "Symbolic tests" $
+  map (\conf ->
+    testContract' "symbolic/verify.sol" Nothing (Just (>= solcV (0,6,9))) (Just conf) True SymbolicWorker
       [ ("simple passed", solved "simple")
       , ("array passed", solved "array")
       , ("negative passed", solved "negative")
       , ("close passed", solved "close")
       , ("far not verified", verified "far")
       , ("correct not verified", verified "correct")
-    ]
+      ]
+  ) ["symbolic/verify.yaml", "symbolic/verify.bitwuzla.yaml"]
+  ++ ([
     -- This test is commented out because it requires a specific setup where both the FuzzWorker and SymbolicWorker are used.
     -- If you run the symbolic worker alone, it will hang indefinitely.
     --, testContract' "symbolic/explore.sol" Nothing Nothing (Just "symbolic/explore.yaml") True SymbolicWorker
     --  [ ("f passed", solved "f")
     --]
-  ]
+  ] :: [TestTree])
