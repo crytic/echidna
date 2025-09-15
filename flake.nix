3f6c281da127156dc93631926439e0e6cd965036
gustavo-grieco <gustavo.grieco+github@gmail.com>
2025-09-15 08:56:54 -0300

    changed flake

--- flake.nix
+++ flake.nix
@@ -54,8 +54,8 @@
           (pkgs.haskellPackages.callCabal2nix "hevm" (pkgs.fetchFromGitHub {
             owner = "ethereum";
             repo = "hevm";
-            rev = "9982c580ed19b88ebab9744d29d940fd2f0bd8c6";
-            sha256 = "sha256-B60aJo9EfIaTWuJaN06Wl1Br+rstzUAYGFBE5rX72Fs=";
+            rev = "87fe0eec5abb69c0b54c097784dfd8712a36de7";
+            sha256 = "sha256-B60aJo9EfIaTWuJaN06Wl1Br+rsXzUAYGFBE5rX72Fs=";
           }) { secp256k1 = pkgs.secp256k1; })
           ([
             pkgs.haskell.lib.compose.dontCheck
