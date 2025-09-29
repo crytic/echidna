b899481b65685d42d2683f758e3d59821e5a9e1f
Gustavo Grieco <31542053+gustavo-grieco@users.noreply.github.com>
2025-09-29 12:45:21 -0300

    Downgrade z3 to 4.12.6 (#1461)
    Some symbolic execution related tests are failing when using z3 (4.15) but work correctly with 4.12.6 for some unknown reason. While we investigate, we will downgrade z3 to have the tests passing (they are also passing with other solvers such as bitwuzla).

--- .github/scripts/install-z3.sh
+++ .github/scripts/install-z3.sh
@@ -3,14 +3,14 @@ set -eux -o pipefail
 
 if [ "$HOST_OS" = "Linux" ]; then
   if [ $(uname -m) = "aarch64" ]; then
-    curl -fsSL -o z3.zip https://github.com/Z3Prover/z3/releases/download/z3-4.15.2/z3-4.15.2-arm64-glibc-2.34.zip
+    curl -fsSL -o z3.zip https://github.com/Z3Prover/z3/releases/download/z3-4.12.6/z3-4.12.6-arm64-glibc-2.35.zip
   elif [ $(uname -m) = "x86_64" ]; then
-    curl -fsSL -o z3.zip https://github.com/Z3Prover/z3/releases/download/z3-4.15.2/z3-4.15.2-x64-glibc-2.39.zip
+    curl -fsSL -o z3.zip https://github.com/Z3Prover/z3/releases/download/z3-4.12.6/z3-4.12.6-x64-glibc-2.35.zip
   fi
   unzip z3.zip
   cp -a z3-*/bin/z3 "$HOME/.local/bin/"
   rm -rf z3-*/ z3.zip
 fi
 if [ "$HOST_OS" = "Windows" ]; then
-  choco install z3
+  choco install z3 --version=4.12.6
 fi
