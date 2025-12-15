e4be047c31d6b9a0a493b9f0bd7b2ac96c014655
Gustavo Grieco <31542053+gustavo-grieco@users.noreply.github.com>
2025-12-15 14:56:43 +0100

    Do not fetch if etherscan key is not available and support different chain ids (#1492)
    * do not fetch if etherscan key is not available and support different chain ids
    
    * etherscan: add support for source maps on other chains
    
    * etherscan: move cache away from Env
    
    * etherscan: remove extra & on URLs
    
    * onchain: remove dead code
    
    ---------
    
    Co-authored-by: Emilio López <emilio.lopez@trailofbits.com>

--- lib/Echidna/Types/Cache.hs
+++ lib/Echidna/Types/Cache.hs
@@ -3,8 +3,6 @@ module Echidna.Types.Cache where
 import Data.Map (Map)
 import Data.Text (Text)
 
-import EVM.Types (W256, Addr, Contract)
+import EVM.Types (W256)
 
-type ContractCache = Map Addr (Maybe Contract)
-type SlotCache = Map Addr (Map W256 (Maybe W256))
 type ContractNameCache = Map W256 Text
