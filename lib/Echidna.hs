a43afb97cf9e27e2a4e16234f835aba9566633f7
gustavo-grieco <gustavo.grieco+github@gmail.com>
2025-12-17 19:40:04 +0100

    first functional MCP command

--- lib/Echidna.hs
+++ lib/Echidna.hs
@@ -133,7 +133,8 @@ mkEnv cfg buildOutput tests world slitherInfo = do
   contractNameCache <- newIORef mempty
   -- TODO put in real path
   let dapp = dappInfo "/" buildOutput
-  pure $ Env { cfg, dapp, codehashMap, fetchSession, contractNameCache
+      sourceCache = buildOutput.sources
+  pure $ Env { cfg, dapp, sourceCache, codehashMap, fetchSession, contractNameCache
              , chainId, eventQueue, bus, coverageRefInit, coverageRefRuntime, corpusRef, testRefs, world
              , slitherInfo
              }
